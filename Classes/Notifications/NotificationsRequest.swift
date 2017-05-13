485b0b66134c7211b4eca2b5f67c3cdc01f32c21
Ryan Nystrom <rnystrom@instagram.com>
2017-05-13 18:13:34 -0400

    setup request

---
+++ Classes/Notifications/NotificationsRequest.swift
@@ -0,0 +1,53 @@
+//
+//  NotificationsRequest.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 5/13/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationResult {
+    case failed(Error?)
+    case noauth
+    case success([Notification])
+}
+
+fileprivate let dateFormatter = DateFormatter()
+
+func requestNotifications(
+    session: GithubSession,
+    all: Bool = false,
+    participating: Bool = false,
+    since: Date = Date(),
+    page: Int = 0,
+    before: Date? = nil,
+    completion: (NotificationResult) -> ()
+    ) {
+    guard let authorization = session.authorization else {
+        completion(.noauth)
+        return
+    }
+    dateFormatter.dateFormat = ""
+
+    var parameters: [String: Any] = [
+        "access_token": authorization.token,
+        "all": all ? "true" : "false",
+        "participating": participating ? "true" : "false",
+        "since": dateFormatter.string(from: since),
+        "page": page,
+        ]
+    if let before = before {
+        parameters["before"] = dateFormatter.string(from: before)
+    }
+
+    let _ = requestGithub(
+        path: "notifications",
+        method: .get,
+        parameters: parameters,
+        headers: nil
+    ) { response in
+        print(response)
+    }
+}
