605e022f570a764018bc72e3ecddf15054b002cc
Ryan Nystrom <rnystrom@instagram.com>
2017-05-13 15:10:23 -0400

    change notification enum

---
+++ Classes/Notifications/NotificationType+Icon.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationType+Icon.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 5/13/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+extension NotificationType {
+
+    var icon: UIImage? {
+        let name: String
+        switch self {
+        case .commit: name = "git-commit"
+        case .issue: name = "issue-opened"
+        case .pullRequest: name = "git-pull-request"
+        }
+        return UIImage(named: name)
+    }
+
+}
