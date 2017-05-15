b4e0b158120dd64332e861ee5e75e64214342233
Ryan Nystrom <rnystrom@instagram.com>
2017-05-14 22:17:30 -0400

    segmented control cell

--- Classes/Views/SegmentedControlCell.swift
+++ Classes/Views/SegmentedControlCell.swift
@@ -7,7 +7,45 @@
 //
 
 import UIKit
+import SnapKit
+
+protocol SegmentedControlCellDelegate: class {
+    func didChangeSelection(cell: SegmentedControlCell, index: Int)
+}
 
 final class SegmentedControlCell: UICollectionViewCell {
+
+    weak var delegate: SegmentedControlCellDelegate? = nil
+
+    private let segmentedControl = UISegmentedControl(frame: .zero)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        segmentedControl.addTarget(self, action: #selector(SegmentedControlCell.didSelect(sender:)), for: .valueChanged)
+        contentView.addSubview(segmentedControl)
+        segmentedControl.snp.makeConstraints { make in
+            make.left.equalTo(Styles.Sizes.gutter)
+            make.right.equalTo(Styles.Sizes.gutter)
+            make.centerY.equalTo(contentView)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public func configure(items: [String]) {
+        segmentedControl.removeAllSegments()
+        for (i, item) in items.enumerated() {
+            segmentedControl.insertSegment(withTitle: item, at: i, animated: false)
+        }
+    }
+
+    // MARK: Private API
+
+    @objc private func didSelect(sender: Any) {
+        delegate?.didChangeSelection(cell: self, index: segmentedControl.selectedSegmentIndex)
+    }
     
 }
