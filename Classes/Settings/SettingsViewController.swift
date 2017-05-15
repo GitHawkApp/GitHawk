21ae778e296b1f5716dafb22ff950989d069a4d6
Ryan Nystrom <rnystrom@instagram.com>
2017-05-15 13:33:59 -0400

    add settings VC and SB

---
+++ Classes/Settings/SettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsViewController.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 5/15/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+final class SettingsViewController: UITableViewController {
+
+    // must be injected after init and before viewDidLoad()
+    var session: GithubSession!
+
+    let signOutIndexPath = IndexPath(item: 0, section: 0)
+
+    // MARK: UITableViewDelegate
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath {
+        case signOutIndexPath: session.remove()
+        default: break
+        }
+    }
+
+}
