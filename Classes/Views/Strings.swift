f99bf3460711df379445d3e39147209f625659e2
Ryan Nystrom <rnystrom@instagram.com>
2017-05-15 11:29:03 -0400

    use string consts

---
+++ Classes/Views/Strings.swift
@@ -0,0 +1,16 @@
+//
+//  Strings.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 5/15/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import Foundation
+
+struct Strings {
+
+    static let all = NSLocalizedString("All", comment: "")
+    static let unread = NSLocalizedString("Unread", comment: "")
+
+}
