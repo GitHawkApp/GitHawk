9763117f32cbf5d0f41a14af2dfc246c1089018f
Ryan Nystrom <rnystrom@instagram.com>
2017-05-16 21:17:48 -0400

    refactor networking to be scoped

--- Classes/Login/LoginViewController.swift
+++ Classes/Login/LoginViewController.swift
@@ -11,7 +11,7 @@ import OnePasswordExtension
 
 final class LoginViewController: UITableViewController, UITextFieldDelegate {
 
-    var session: GithubSession!
+    var client: GithubClient!
 
     let signinCellIndexPath = IndexPath(item: 0, section: 2)
 
@@ -33,7 +33,7 @@ final class LoginViewController: UITableViewController, UITextFieldDelegate {
         if let controller = segue.destination as? TwoFactorViewController {
             controller.username = usernameTextField.text
             controller.password = passwordTextField.text
-            controller.session = session
+            controller.client = client
         }
     }
 
@@ -75,7 +75,7 @@ final class LoginViewController: UITableViewController, UITextFieldDelegate {
         showLoadingIndicator(true)
         viewsEnabled(false)
 
-        requestGithubLogin(username: username, password: password) { result in
+        client.requestGithubLogin(username: username, password: password) { result in
             self.showLoadingIndicator(false)
             self.viewsEnabled(true)
             self.handleResult(result)
@@ -85,7 +85,7 @@ final class LoginViewController: UITableViewController, UITextFieldDelegate {
     func handleResult(_ result: GithubLogin) {
         switch result {
         case .success(let auth):
-            session.add(authorization: auth)
+            client.session.add(authorization: auth)
         case .failed:
             let title = NSLocalizedString("Sign in Error", comment: "")
             let message = NSLocalizedString("Username or password are incorrect.", comment: "")
---
+++ Classes/Systems/GithubClient.swift
@@ -0,0 +1,75 @@
+//
+//  GithubClient.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 5/16/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct GithubClient {
+
+    struct Request {
+        let path: String
+        let method: HTTPMethod
+        let parameters: Parameters?
+        let headers: HTTPHeaders?
+        let completion: (DataResponse<Any>) -> Void
+
+        init(
+            path: String,
+            method: HTTPMethod = .get,
+            parameters: Parameters? = nil,
+            headers: HTTPHeaders? = nil,
+            completion: @escaping (DataResponse<Any>) -> Void
+            ) {
+            self.path = path
+            self.method = method
+            self.parameters = parameters
+            self.headers = headers
+            self.completion = completion
+        }
+    }
+
+    let session: GithubSession
+    let networker: Alamofire.SessionManager
+
+    @discardableResult
+    func request(
+        _ request: GithubClient.Request
+        ) -> DataRequest {
+        print("Requesting: " + request.path)
+
+        let encoding: ParameterEncoding
+        switch request.method {
+        case .get: encoding = URLEncoding.queryString
+        default: encoding = JSONEncoding.default
+        }
+
+        var parameters = request.parameters ?? [:]
+        if let authorization = session.authorization {
+            parameters["access_token"] = authorization.token
+        }
+
+        return networker.request("https://api.github.com/" + request.path,
+                                 method: request.method,
+                                 parameters: parameters,
+                                 encoding: encoding,
+                                 headers: request.headers)
+            .responseJSON(completionHandler: { response in
+                print(response.response ?? "")
+                print(response.value ?? response.error?.localizedDescription ?? "Unknown error")
+
+                // remove the github session if requesting with a session
+                if let statusCode = response.response?.statusCode,
+                    (statusCode == 401 || statusCode == 403) {
+                    self.session.remove()
+                } else {
+                    request.completion(response)
+                }
+            })
+    }
+    
+}
