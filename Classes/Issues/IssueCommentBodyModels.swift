586bbf92291aa05b98b98bcc663cbce3abf95daf
Ryan Nystrom <rnystrom@instagram.com>
2017-05-22 10:22:54 -0400

    image url scanning tests pass

--- Classes/Issues/IssueCommentBodyModels.swift
+++ Classes/Issues/IssueCommentBodyModels.swift
@@ -12,10 +12,13 @@ import IGListKit
 private func sizingString(
     body: String,
     width: CGFloat,
-    startIndex: String.Index,
-    endIndex: String.Index
-    ) -> NSAttributedStringSizing {
-    let between = body.substring(with: startIndex..<endIndex)
+    start: Int,
+    end: Int
+    ) -> NSAttributedStringSizing? {
+    guard let between = body
+        .substring(with: NSRange(location: start, length: end - start))?
+        .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) else { return nil }
+
     let attributedString = NSAttributedString(
         string: between,
         attributes: [
@@ -29,34 +32,69 @@ private func sizingString(
     )
 }
 
-private let imageRegex = try! NSRegularExpression(pattern: "!\\[.+]\\((.+)\\)", options: [])
+private let imageRegex = try! NSRegularExpression(pattern: "!\\[.+]\\((.+)\\)", options: [.useUnixLineSeparators])
+
+func imageURLMatches(body: String) -> [NSTextCheckingResult] {
+    return imageRegex.matches(in: body, options: [], range: body.nsrange)
+}
+
+// http://nshipster.com/nsregularexpression/
+extension String {
+    /// An `NSRange` that represents the full range of the string.
+    var nsrange: NSRange {
+        return NSRange(location: 0, length: utf16.count)
+    }
+
+    /// Returns a substring with the given `NSRange`,
+    /// or `nil` if the range can't be converted.
+    func substring(with nsrange: NSRange) -> String? {
+        guard let range = nsrange.toRange()
+            else { return nil }
+        let start = UTF16Index(range.lowerBound)
+        let end = UTF16Index(range.upperBound)
+        return String(utf16[start..<end])
+    }
+
+    /// Returns a range equivalent to the given `NSRange`,
+    /// or `nil` if the range can't be converted.
+    func range(from nsrange: NSRange) -> Range<Index>? {
+        guard let range = nsrange.toRange() else { return nil }
+        let utf16Start = UTF16Index(range.lowerBound)
+        let utf16End = UTF16Index(range.upperBound)
+
+        guard let start = Index(utf16Start, within: self),
+            let end = Index(utf16End, within: self)
+            else { return nil }
+
+        return start..<end
+    }
+}
 
 func createCommentModels(body: String, width: CGFloat) -> [IGListDiffable] {
 
     var result = [IGListDiffable]()
 
-    let matches = imageRegex.matches(in: body, options: [], range: NSRange(location: 0, length: body.characters.count))
-    var location = body.startIndex
+    let matches = imageURLMatches(body: body)
+    var location = 0
 
     for match in matches {
-        let betweenEnd = body.index(body.startIndex, offsetBy: match.range.location)
-        let sizing = sizingString(body: body, width: width, startIndex: location, endIndex: betweenEnd)
-        result.append(sizing)
+        if match.range.location > 0,
+            let sizing = sizingString(body: body, width: width, start: location, end: match.range.location) {
+            result.append(sizing)
+        }
 
-        location = body.index(betweenEnd, offsetBy: match.range.length)
+        location = match.range.location + match.range.length
 
-        let imageRange = match.rangeAt(1)
-        let imageBegin = body.index(body.startIndex, offsetBy: imageRange.location)
-        let imageEnd = body.index(imageBegin, offsetBy: imageRange.length)
-        let imageURLString = body.substring(with: imageBegin..<imageEnd)
-        if let url = URL(string: imageURLString) {
+        if let string = body.substring(with: match.rangeAt(1)), let url = URL(string: string) {
             result.append(IssueCommentImageModel(url: url))
         }
     }
 
-    let remaining = sizingString(body: body, width: width, startIndex: location, endIndex: body.endIndex)
-    result.append(remaining)
-    
+    let end = body.utf16.count
+    if end - location > 0,
+        let remaining = sizingString(body: body, width: width, start: location, end: end) {
+        result.append(remaining)
+    }
+
     return result
 }
-
--- FreetimeTests/IssueTests.swift
+++ FreetimeTests/IssueTests.swift
@@ -10,12 +10,60 @@ import XCTest
 
 class IssueTests: XCTestCase {
 
+    func test_whenSearchingForImageURL_withOneImage() {
+        let body = "foo ![alt](https://apple.com) bar"
+        let result = imageURLMatches(body: body)
+        XCTAssertEqual(result.count, 1)
+        XCTAssertEqual(result[0].range.location, 4)
+        XCTAssertEqual(result[0].range.length, 25)
+        XCTAssertEqual(result[0].numberOfRanges, 2)
+        XCTAssertEqual(result[0].rangeAt(1).location, 11)
+        XCTAssertEqual(result[0].rangeAt(1).length, 17)
+    }
+
+    func test_whenSearchingForImageURL_withOneImage_withNewlines() {
+        let body = "foo\r\n![alt](https://apple.com)\r\nbar"
+        let result = imageURLMatches(body: body)
+        XCTAssertEqual(result.count, 1)
+        XCTAssertEqual(result[0].range.location, 5)
+        XCTAssertEqual(result[0].range.length, 25)
+        XCTAssertEqual(result[0].numberOfRanges, 2)
+        XCTAssertEqual(result[0].rangeAt(1).location, 12)
+        XCTAssertEqual(result[0].rangeAt(1).length, 17)
+    }
+
+    func test_whenPluckingSubstringFromRange() {
+        let result = "foo ![alt](https://apple.com) bar".substring(with: NSRange(location: 11, length: 17))
+        XCTAssertEqual(result, "https://apple.com")
+    }
+
+    func test_whenPluckingSubstring_withNewlines() {
+        let body = [
+            "this is the first line",
+            "![alt text](https://apple.com)",
+            "then some more text",
+            ].joined(separator: "\r\n")
+        let result = body.substring(with: NSRange(location: 36, length: 17))
+        XCTAssertEqual(result, "https://apple.com")
+    }
+
+    func test_whenPluckingImageURLSubstring_withNewlines() {
+        let body = [
+            "this is the first line",
+            "![alt text](https://apple.com)",
+            "then some more text",
+            ].joined(separator: "\r\n")
+        let range = imageURLMatches(body: body)[0].rangeAt(1)
+        let result = body.substring(with: range)
+        XCTAssertEqual(result, "https://apple.com")
+    }
+
     func test_whenImageInTheMiddle() {
         let body = [
             "this is the first line",
             "![alt text](https://apple.com)",
             "then some more text",
-            ].joined(separator: "\\r\\n")
+            ].joined(separator: "\r\n")
         let models = createCommentModels(body: body, width: 300)
         XCTAssertEqual(models.count, 3)
         XCTAssertEqual((models[0] as! NSAttributedStringSizing).attributedText.string, "this is the first line")
@@ -28,12 +76,11 @@ class IssueTests: XCTestCase {
             "![alt text](https://apple.com)",
             "this is the first line",
             "then some more text",
-            ].joined(separator: "\\r\\n")
+            ].joined(separator: "\r\n")
         let models = createCommentModels(body: body, width: 300)
-        XCTAssertEqual(models.count, 3)
+        XCTAssertEqual(models.count, 2)
         XCTAssertEqual((models[0] as! IssueCommentImageModel).url.absoluteString, "https://apple.com")
-        XCTAssertEqual((models[1] as! NSAttributedStringSizing).attributedText.string, "this is the first line")
-        XCTAssertEqual((models[2] as! NSAttributedStringSizing).attributedText.string, "then some more text")
+        XCTAssertEqual((models[1] as! NSAttributedStringSizing).attributedText.string, "this is the first line\r\nthen some more text")
     }
 
     func test_whenImageAtTheEnd() {
@@ -41,18 +88,17 @@ class IssueTests: XCTestCase {
             "this is the first line",
             "then some more text",
             "![alt text](https://apple.com)",
-            ].joined(separator: "\\r\\n")
+            ].joined(separator: "\r\n")
         let models = createCommentModels(body: body, width: 300)
-        XCTAssertEqual(models.count, 3)
-        XCTAssertEqual((models[0] as! NSAttributedStringSizing).attributedText.string, "this is the first line")
-        XCTAssertEqual((models[1] as! NSAttributedStringSizing).attributedText.string, "then some more text")
-        XCTAssertEqual((models[2] as! IssueCommentImageModel).url.absoluteString, "https://apple.com")
+        XCTAssertEqual(models.count, 2)
+        XCTAssertEqual((models[0] as! NSAttributedStringSizing).attributedText.string, "this is the first line\r\nthen some more text")
+        XCTAssertEqual((models[1] as! IssueCommentImageModel).url.absoluteString, "https://apple.com")
     }
 
     func test_whenOnlyOneImage() {
         let body = [
             "![alt text](https://apple.com)",
-            ].joined(separator: "\\r\\n")
+            ].joined(separator: "\r\n")
         let models = createCommentModels(body: body, width: 300)
         XCTAssertEqual(models.count, 1)
         XCTAssertEqual((models[0] as! IssueCommentImageModel).url.absoluteString, "https://apple.com")
@@ -65,7 +111,7 @@ class IssueTests: XCTestCase {
             "then some more text",
             "![alt text](https://google.com)",
             "foo bar baz",
-            ].joined(separator: "\\r\\n")
+            ].joined(separator: "\r\n")
         let models = createCommentModels(body: body, width: 300)
         XCTAssertEqual(models.count, 5)
         XCTAssertEqual((models[0] as! NSAttributedStringSizing).attributedText.string, "this is the first line")
