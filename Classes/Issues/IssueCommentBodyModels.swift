5baabe6dc60218652f06776f79ad6bb9c2461a75
Ryan Nystrom <rnystrom@instagram.com>
2017-05-22 22:23:30 -0400

    code blocks

--- Classes/Issues/IssueCommentBodyModels.swift
+++ Classes/Issues/IssueCommentBodyModels.swift
@@ -10,6 +10,41 @@ import Foundation
 import IGListKit
 import CocoaMarkdown
 
+private func markdownString(
+    body: String,
+    width: CGFloat,
+    attributes: CMTextAttributes,
+    inset: UIEdgeInsets
+    ) -> NSAttributedStringSizing? {
+    let data = body.data(using: .utf8)
+    guard let document = CMDocument(data: data, options: [.hardBreaks]),
+        let attributedString = document.attributedString(with: attributes)
+        else { return nil }
+
+    return NSAttributedStringSizing(
+        containerWidth: width,
+        attributedText: attributedString,
+        inset: inset
+    )
+}
+
+private func codeBlockString(
+    _ body: String
+    ) -> NSAttributedStringSizing {
+    let attributes: [String: Any] = [
+        NSForegroundColorAttributeName: Styles.Colors.Gray.dark,
+        NSFontAttributeName: Styles.Fonts.code
+    ]
+    let attributedText = NSAttributedString(
+        string: body.trimmingCharacters(in: .whitespacesAndNewlines),
+        attributes: attributes
+    )
+    return NSAttributedStringSizing(
+        containerWidth: CGFloat.greatestFiniteMagnitude,
+        attributedText: attributedText
+    )
+}
+
 private func bodyString(
     body: String,
     width: CGFloat,
@@ -20,49 +55,73 @@ private func bodyString(
         end > 0,
         end - start > 0,
         let between = body
-        .substring(with: NSRange(location: start, length: end - start))?
-        .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) else { return nil }
+            .substring(with: NSRange(location: start, length: end - start))?
+            .trimmingCharacters(in: .whitespacesAndNewlines),
+        let textAttributes = CMTextAttributes()
+        else { return nil }
 
-    let data = between.data(using: .utf8)
-    let document = CMDocument(data: data, options: [])
-
-    let textAttributes = CMTextAttributes()
-    textAttributes?.textAttributes = [
+    textAttributes.textAttributes = [
         NSForegroundColorAttributeName: Styles.Colors.Gray.dark,
         NSFontAttributeName: Styles.Fonts.body,
     ]
-    textAttributes?.linkAttributes = [
+    textAttributes.linkAttributes = [
         NSForegroundColorAttributeName: Styles.Colors.blue,
-        NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue
+        NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue,
     ]
-    textAttributes?.inlineCodeAttributes = [
+    textAttributes.inlineCodeAttributes = [
         NSForegroundColorAttributeName: Styles.Colors.Gray.dark,
         NSBackgroundColorAttributeName: Styles.Colors.Gray.lighter,
-        NSFontAttributeName: Styles.Fonts.code
+        NSFontAttributeName: Styles.Fonts.code,
+    ]
+    textAttributes.orderedListAttributes = [
+        NSParagraphStyleAttributeName: NSParagraphStyle(),
+    ]
+    textAttributes.unorderedListAttributes = [
+        NSParagraphStyleAttributeName: NSParagraphStyle(),
     ]
 
-    let listStyle = NSMutableParagraphStyle()
-    textAttributes?.orderedListAttributes = [
-        NSParagraphStyleAttributeName: listStyle
-    ]
-    textAttributes?.unorderedListAttributes = [
-        NSParagraphStyleAttributeName: listStyle
-    ]
-
-    let attributedString = document!.attributedString(with: textAttributes)!
-
-    return NSAttributedStringSizing(
-        containerWidth: width,
-        attributedText: attributedString,
+    return markdownString(
+        body: between,
+        width: width,
+        attributes: textAttributes,
         inset: IssueCommentTextCell.inset
     )
 }
 
 private let imageRegex = try! NSRegularExpression(pattern: "!\\[.+]\\((.+)\\)", options: [.useUnixLineSeparators])
+private let blockCodeRegex = try! NSRegularExpression(pattern: "(?s)```([a-zA-Z0-9-]+)?(.*?)```", options: [.useUnixLineSeparators])
 
 func imageURLMatches(body: String) -> [NSTextCheckingResult] {
     return imageRegex.matches(in: body, options: [], range: body.nsrange)
 }
+
+struct BodyScanner {
+
+    let regex: NSRegularExpression
+    let handler: (String, CGFloat, NSTextCheckingResult) -> IGListDiffable?
+
+}
+
+let imageScanner = BodyScanner(
+    regex: try! NSRegularExpression(pattern: "!\\[.+]\\((.+)\\)", options: [.useUnixLineSeparators]),
+    handler: { (body, width, match) in
+        if let string = body.substring(with: match.rangeAt(1)), let url = URL(string: string) {
+            return IssueCommentImageModel(url: url)
+        }
+        return nil
+}
+)
+
+let codeBlockScanner = BodyScanner(
+    regex: try! NSRegularExpression(pattern: "```([a-zA-Z0-9-]+)?(.*?)```", options: [.useUnixLineSeparators, .dotMatchesLineSeparators]),
+    handler: { (body, width, match) in
+        let language = body.substring(with: match.rangeAt(1))
+        if let code = body.substring(with: match.rangeAt(2)) {
+            return IssueCommentCodeBlockModel(code: codeBlockString(code), language: language)
+        }
+        return nil
+}
+)
 
 // http://nshipster.com/nsregularexpression/
 extension String {
@@ -98,27 +157,38 @@ extension String {
 
 func createCommentModels(body: String, width: CGFloat) -> [IGListDiffable] {
 
-    var result = [IGListDiffable]()
+    let newlineCleanedBody = body.replacingOccurrences(of: "\r\n", with: "\n")
 
-    let matches = imageURLMatches(body: body)
+    var results = [IGListDiffable]()
+
+    var matchPairs = [ (NSTextCheckingResult, BodyScanner) ]()
+
+    for scanner in [imageScanner, codeBlockScanner] {
+        let matches = scanner.regex.matches(in: newlineCleanedBody, options: [], range: newlineCleanedBody.nsrange)
+        let pairs = matches.map { ($0, scanner) }
+        matchPairs += pairs
+    }
+
+    matchPairs.sort { $0.0.range.location < $1.0.range.location }
+
     var location = 0
 
-    for match in matches {
-        if let sizing = bodyString(body: body, width: width, start: location, end: match.range.location) {
-            result.append(sizing)
+    for (match, scanner) in matchPairs {
+        if let sizing = bodyString(body: newlineCleanedBody, width: width, start: location, end: match.range.location) {
+            results.append(sizing)
         }
 
         location = match.range.location + match.range.length
 
-        if let string = body.substring(with: match.rangeAt(1)), let url = URL(string: string) {
-            result.append(IssueCommentImageModel(url: url))
+        if let result = scanner.handler(newlineCleanedBody, width, match) {
+            results.append(result)
         }
     }
 
-    let end = body.utf16.count
-    if let remaining = bodyString(body: body, width: width, start: location, end: end) {
-        result.append(remaining)
+    let end = newlineCleanedBody.utf16.count
+    if let remaining = bodyString(body: newlineCleanedBody, width: width, start: location, end: end) {
+        results.append(remaining)
     }
-
-    return result
+    
+    return results
 }
