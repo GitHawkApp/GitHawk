057f10e24e66a80cbbe7c46d2a91ac98eeaa9074
Ryan Nystrom <rnystrom@instagram.com>
2017-05-28 00:28:47 -0400

    shared height function, function to calculate cap index and height

---
+++ Classes/Issues/IssueCommentViewModelHeight.swift
@@ -0,0 +1,21 @@
+//
+//  IssueCommentViewModelHeight.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 5/28/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+func bodyHeight(viewModel: Any) -> CGFloat {
+    if let viewModel = viewModel as? NSAttributedStringSizing {
+        return viewModel.textViewSize.height
+    } else if let viewModel = viewModel as? IssueCommentCodeBlockModel {
+        return viewModel.code.textViewSize.height
+    } else if viewModel is IssueCommentImageModel {
+        return 200.0
+    } else {
+        return Styles.Sizes.tableCellHeight
+    }
+}
