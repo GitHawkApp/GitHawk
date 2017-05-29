0762773bf267e931c8ac4013b24367af4c4d6bef
Ryan Nystrom <rnystrom@instagram.com>
2017-05-29 14:21:35 -0400

    min delta height for collapsing

--- Classes/Issues/IssueViewModels.swift
+++ Classes/Issues/IssueViewModels.swift
@@ -47,11 +47,14 @@ func createIssueTitleString(issue: Issue, width: CGFloat) -> NSAttributedStringS
 
 func collapsedBodyInfo(bodies: [AnyObject]) -> (AnyObject, CGFloat)? {
     let cap: CGFloat = 300
+    // minimum height to collapse so expanding shows significant amount of content
+    let minDelta = CollapseCellMinHeight * 3
+
     var totalHeight: CGFloat = 0
     for body in bodies {
         let height = bodyHeight(viewModel: body)
         totalHeight += height
-        if totalHeight > cap {
+        if totalHeight > cap, totalHeight - cap > minDelta {
             let collapsedBodyHeight = max(cap - (totalHeight - height), CollapseCellMinHeight)
             return (body, collapsedBodyHeight)
         }
