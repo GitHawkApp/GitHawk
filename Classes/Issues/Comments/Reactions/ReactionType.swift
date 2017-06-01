93c710d9ca366f097a46ab480b2f4ba0db76c6a7
Ryan Nystrom <rnystrom@instagram.com>
2017-06-01 00:18:56 -0400

    reaction types

---
+++ Classes/Issues/Comments/Reactions/ReactionType.swift
@@ -0,0 +1,20 @@
+//
+//  ReactionType.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 6/1/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import Foundation
+
+enum ReactionType: String {
+
+    case thumbsUp = "👍"
+    case hooray = "🎉"
+    case thumbsDown = "👎"
+    case heart = "❤️"
+    case laugh = "😄"
+    case confused = "😕"
+
+}
