22eeac775a0e7c3d121181eb43d2d4e07ad4edfe
Ryan Nystrom <rnystrom@instagram.com>
2017-06-01 00:27:24 -0400

    make rx view models

---
+++ Classes/Issues/Comments/Reactions/ReactionViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ReactionViewModel.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 6/1/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import Foundation
+
+struct ReactionViewModel {
+
+    let type: ReactionType
+    let count: Int
+    let viewerDidReact: Bool
+
+}
