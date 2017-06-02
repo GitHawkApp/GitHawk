79885c048f698627394826c6b444943428206b6d
Ryan Nystrom <rnystrom@instagram.com>
2017-06-02 17:51:39 -0400

    wip reaction buttons

---
+++ Classes/Views/ResponderButton.swift
@@ -0,0 +1,17 @@
+//
+//  ResponderButton.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 6/2/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+final class ResponderButton: UIButton {
+
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+
+}
