048e7289a5b4ca0a025d93632caedfcec9be1fec
Ryan Nystrom <rnystrom@instagram.com>
2017-06-22 23:37:32 -0400

    start a webview on app start to speed things up

--- Classes/Systems/AppDelegate.swift
+++ Classes/Systems/AppDelegate.swift
@@ -24,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // initialize a webview at the start so webview startup later on isn't so slow
+        let _ = UIWebView()
         rootNavigationManager.resetRootViewController(userSession: sessionManager.focusedUserSession)
         return true
     }
