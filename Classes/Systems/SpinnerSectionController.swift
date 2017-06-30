de9b5bd80d4e86b4c754503e52c54435ac87f437
Ryan Nystrom <rnystrom@instagram.com>
2017-06-30 08:25:57 -0700

    spinner cell and SC

---
+++ Classes/Systems/SpinnerSectionController.swift
@@ -0,0 +1,25 @@
+//
+//  SpinnerSectionController.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 6/30/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+import IGListKit
+
+final class SpinnerSectionController: ListSectionController {
+
+    override func sizeForItem(at index: Int) -> CGSize {
+        guard let width = collectionContext?.containerSize.width else { fatalError("Missing context") }
+        return CGSize(width: width, height: Styles.Sizes.tableCellHeight)
+    }
+
+    override func cellForItem(at index: Int) -> UICollectionViewCell {
+        guard let cell = collectionContext?.dequeueReusableCell(of: SpinnerCell.self, for: self, at: index) as? SpinnerCell
+            else { fatalError("Missing context or cell wrong type") }
+        return cell
+    }
+
+}
---
+++ Classes/Views/SpinnerCell.swift
@@ -0,0 +1,30 @@
+//
+//  SpinnerCell.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 6/30/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+final class SpinnerCell: UICollectionViewCell {
+
+    let activity = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        activity.startAnimating()
+        contentView.addSubview(activity)
+        activity.snp.makeConstraints { make in
+            make.center.equalTo(contentView)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
