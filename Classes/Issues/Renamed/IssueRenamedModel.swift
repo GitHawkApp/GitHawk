361c17cbdfadca276684fd4887a11d8c02b09634
Ryan Nystrom <rnystrom@instagram.com>
2017-07-12 00:51:30 -0400

    renamed events, fixes #51

---
+++ Classes/Issues/Renamed/IssueRenamedModel.swift
@@ -0,0 +1,41 @@
+//
+//  IssueRenamedEvent.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 7/12/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import Foundation
+import IGListKit
+
+final class IssueRenamedModel: ListDiffable {
+
+    let id: String
+    let actor: String
+    let date: Date
+    let titleChangeString: NSAttributedStringSizing
+
+    init(
+        id: String,
+        actor: String,
+        date: Date,
+        titleChangeString: NSAttributedStringSizing
+        ) {
+        self.id = id
+        self.actor = actor
+        self.date = date
+        self.titleChangeString = titleChangeString
+    }
+
+    // MARK: ListDiffable
+
+    func diffIdentifier() -> NSObjectProtocol {
+        return id as NSObjectProtocol
+    }
+
+    func isEqual(toDiffableObject object: ListDiffable?) -> Bool {
+        return true
+    }
+
+}
---
+++ Classes/Issues/Renamed/IssueRenamedString.swift
@@ -0,0 +1,29 @@
+//
+//  IssueRenamedString.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 7/12/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+func IssueRenamedString(previous: String, current: String, width: CGFloat) -> NSAttributedStringSizing {
+    let titleAttributes = [
+        NSForegroundColorAttributeName: Styles.Colors.Gray.dark.color,
+        NSFontAttributeName: Styles.Fonts.bodyBold
+    ]
+    let dividerAttributes = [
+        NSForegroundColorAttributeName: Styles.Colors.Gray.light.color,
+        NSFontAttributeName: Styles.Fonts.body
+    ]
+    let text = NSMutableAttributedString(string: previous, attributes: titleAttributes)
+    text.append(NSAttributedString(string: NSLocalizedString(" to ", comment: ""), attributes: dividerAttributes))
+    text.append(NSAttributedString(string: current, attributes: titleAttributes))
+        return NSAttributedStringSizing(
+        containerWidth: width,
+        attributedText: text,
+        inset: IssueRenamedCell.titleInset,
+        backgroundColor: Styles.Colors.background
+    )
+}
