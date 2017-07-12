d5c94935a220a1c2375d2ef8be99ebc0e9731339
Ryan Nystrom <rnystrom@instagram.com>
2017-07-12 00:51:30 -0400

    renamed events, fixes #51

---
+++ Classes/Issues/Renamed/IssueRenamedCell.swift
@@ -0,0 +1,77 @@
+//
+//  IssueRenamedCell.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 7/12/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+final class IssueRenamedCell: UICollectionViewCell {
+
+    static let titleInset = UIEdgeInsets(
+        top: 24,
+        left: Styles.Sizes.gutter,
+        bottom: Styles.Sizes.rowSpacing,
+        right: Styles.Sizes.gutter
+    )
+
+    let actorLabel = UILabel()
+    let dateLabel = ShowMoreDetailsLabel()
+    let titleView = AttributedStringView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        contentView.addSubview(actorLabel)
+        actorLabel.snp.makeConstraints { make in
+            make.left.equalTo(Styles.Sizes.gutter)
+            make.top.equalTo(0)
+        }
+
+        dateLabel.font = Styles.Fonts.body
+        dateLabel.textColor = Styles.Colors.Gray.medium.color
+        contentView.addSubview(dateLabel)
+        dateLabel.snp.makeConstraints { make in
+            make.left.equalTo(actorLabel.snp.right).offset(Styles.Sizes.inlineSpacing)
+            make.bottom.equalTo(actorLabel)
+        }
+
+        contentView.addSubview(titleView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        titleView.reposition(width: contentView.bounds.width)
+    }
+
+    // MARK: Public API
+
+    func configure(_ model: IssueRenamedModel) {
+        let actorAttributes = [
+            NSFontAttributeName: Styles.Fonts.bodyBold,
+            NSForegroundColorAttributeName: Styles.Colors.Gray.dark.color
+        ]
+        let actor = NSMutableAttributedString(string: model.actor, attributes: actorAttributes)
+        let referencedAttributes = [
+            NSFontAttributeName: Styles.Fonts.body,
+            NSForegroundColorAttributeName: Styles.Colors.Gray.medium.color
+        ]
+        actor.append(NSAttributedString(
+            string: NSLocalizedString(" renamed", comment: ""),
+            attributes: referencedAttributes
+            ))
+        actorLabel.attributedText = actor
+
+        dateLabel.setText(date: model.date)
+
+        titleView.configureAndSizeToFit(text: model.titleChangeString, width: contentView.bounds.width)
+    }
+
+}
