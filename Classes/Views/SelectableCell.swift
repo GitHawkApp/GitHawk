5673a867a918e06559d909a35b7ab5b41871ffdb
Bas Broek <bas@basbroek.nl>
2017-07-12 10:32:53 +0200

    de-implement init with coder

--- Classes/Views/SelectableCell.swift
+++ Classes/Views/SelectableCell.swift
@@ -47,7 +47,7 @@ class SelectableCell: UICollectionViewCell {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
     }
 
     override func prepareForReuse() {
