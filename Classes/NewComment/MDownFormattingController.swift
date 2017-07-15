882324f4cc94ef275d587f05ffc7140097dbe1cb
Florent Vilmart <florent@flovilmart.com>
2017-07-15 15:25:25 -0400

    Adds ability to insert headers on start of lines

--- Classes/NewComment/MDownFormattingController.swift
+++ Classes/NewComment/MDownFormattingController.swift
@@ -4,15 +4,28 @@ import UIKit
 private struct MDownFormattingElement {
     let name: String
     let replacements: (String, String?) // before / after
+    let atLineStart: Bool
+
+    init(name: String, replacements: (String, String?)) {
+        self.name = name
+        self.replacements = replacements
+        self.atLineStart = false
+    }
+
+    init(name: String, replacements: (String, String?), atLineStart: Bool) {
+        self.name = name
+        self.replacements = replacements
+        self.atLineStart = atLineStart
+    }
 }
 
 private let baseElements = [
     MDownFormattingElement(name: "mdown.codeBlock", replacements: ("```\n", "\n```")),
     MDownFormattingElement(name: "mdown.code", replacements: ("`", "`")),
-    MDownFormattingElement(name: "H1", replacements: ("# ", nil)),
-    MDownFormattingElement(name: "H2", replacements: ("## ", nil)),
-    MDownFormattingElement(name: "H3", replacements: ("### ", nil)),
-    MDownFormattingElement(name: "H4", replacements: ("#### ", nil)),
+    MDownFormattingElement(name: "H1", replacements: ("# ", nil), atLineStart: true),
+    MDownFormattingElement(name: "H2", replacements: ("## ", nil), atLineStart: true),
+    MDownFormattingElement(name: "H3", replacements: ("### ", nil), atLineStart: true),
+    MDownFormattingElement(name: "H4", replacements: ("#### ", nil), atLineStart: true),
     MDownFormattingElement(name: "mdown.bold", replacements: ("**", "**")),
     MDownFormattingElement(name: "mdown.emphasis", replacements: ("*", "*")),
     MDownFormattingElement(name: "mdown.strike", replacements: ("~~", "~~"))
@@ -88,7 +101,15 @@ class MDownFormattingController: UIViewController {
         let elt = elements[idx]
         let before = elt.replacements.0
         let after = elt.replacements.1 ?? ""
-        textView.replace(range, withText: "\(before)\(text)\(after)")
+        let replacementText = "\(before)\(text)\(after)"
+
+        var insertionRange = range
+        if elt.atLineStart {
+            let startLinePosition = textView.startOfLine(forRange: range)
+            insertionRange = textView.textRange(from: startLinePosition, to: startLinePosition) ?? range
+        }
+
+        textView.replace(insertionRange, withText: replacementText)
         if range.start == range.end, // single cursor (no selection)
             let position = textView.position(from: range.start, // advance by the inserted before
                                              offset: before.lengthOfBytes(using: .utf8)) {
@@ -96,3 +117,39 @@ class MDownFormattingController: UIViewController {
         }
     }
 }
+
+extension UITextView {
+
+    private func oneCharRange(pos: UITextPosition?) -> UITextRange? {
+        guard let pos = pos,
+            let position = self.position(from: pos, offset: 1) else { return nil }
+        return self.textRange(from: pos, to: position)
+    }
+
+    private func text(atPosition position: UITextPosition?) -> String? {
+        guard let position = position,
+            let range = oneCharRange(pos: position) else { return nil }
+        return text(in: range)
+    }
+
+    func startOfLine(forRange range: UITextRange) -> UITextPosition {
+
+        func previousPosition(pos: UITextPosition?) -> UITextPosition? {
+            guard let pos = pos else { return nil }
+            return self.position(from: pos, offset: -1)
+        }
+
+        var position: UITextPosition? =  previousPosition(pos: range.start)
+        while let text = text(atPosition: position), text != "\n" { // check if it's the EoL
+            position = previousPosition(pos: position) // move back 1 char
+        }
+
+        if let position = position, // we have a position
+            let pos = self.position(from: position, offset: 1) { // need to advance by one...
+            return pos
+        }
+
+        return beginningOfDocument // not found? Go to the beginning
+    }
+}
+
