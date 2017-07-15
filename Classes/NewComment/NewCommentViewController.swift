0eacfbd1ee471bb9c72260ee12a3b1ce95332731
Ryan Nystrom <rnystrom@instagram.com>
2017-07-15 01:19:03 -0400

    very basic writing working

--- Classes/NewComment/NewCommentViewController.swift
+++ Classes/NewComment/NewCommentViewController.swift
@@ -8,10 +8,9 @@
 
 import UIKit
 
-final class NewCommentViewController: UIViewController, UITextViewDelegate {
+final class NewCommentViewController: UIViewController, UITextViewDelegate, AddCommentListener {
 
-    
-
+    private let client: AddCommentClient
     private let placeholderLabel = UILabel()
     private let textView = UITextView()
     private let textViewInsets = UIEdgeInsets(
@@ -20,6 +19,16 @@ final class NewCommentViewController: UIViewController, UITextViewDelegate {
         bottom: 0,
         right: Styles.Sizes.gutter + Styles.Sizes.gutter
     )
+
+    init(client: AddCommentClient) {
+        self.client = client
+        super.init(nibName: nil, bundle: nil)
+        client.addListener(listener: self)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,12 +41,7 @@ final class NewCommentViewController: UIViewController, UITextViewDelegate {
             target: self,
             action: #selector(NewCommentViewController.onCancel)
         )
-        navigationItem.rightBarButtonItem = UIBarButtonItem(
-            title: NSLocalizedString("Send", comment: ""),
-            style: .done,
-            target: self,
-            action: #selector(NewCommentViewController.onSend)
-        )
+        update(sending: false)
 
         NotificationCenter.default.addObserver(
             self,
@@ -65,7 +69,7 @@ final class NewCommentViewController: UIViewController, UITextViewDelegate {
         textView.backgroundColor = .clear
         view.addSubview(textView)
 
-        updateState()
+        updateTextState()
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -86,6 +90,27 @@ final class NewCommentViewController: UIViewController, UITextViewDelegate {
 
     // MARK: Private API
 
+    func update(sending: Bool) {
+        textView.isEditable = !sending
+
+        if sending {
+            textView.resignFirstResponder()
+        }
+
+        if sending {
+            let activity = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+            activity.startAnimating()
+            navigationItem.rightBarButtonItem = UIBarButtonItem(customView: activity)
+        } else {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(
+                title: NSLocalizedString("Send", comment: ""),
+                style: .done,
+                target: self,
+                action: #selector(NewCommentViewController.onSend)
+            )
+        }
+    }
+
     func restoreTextViewInsets() {
         textView.textContainerInset = textViewInsets
         textView.scrollIndicatorInsets = .zero
@@ -95,7 +120,7 @@ final class NewCommentViewController: UIViewController, UITextViewDelegate {
         return textView.text.characters.count > 0
     }
 
-    func updateState() {
+    func updateTextState() {
         let hasText = self.hasText
         navigationItem.rightBarButtonItem?.isEnabled = hasText
         placeholderLabel.isHidden = hasText
@@ -124,7 +149,8 @@ final class NewCommentViewController: UIViewController, UITextViewDelegate {
     }
 
     func onSend() {
-
+        update(sending: true)
+        client.addComment(body: textView.text)
     }
 
     func onKeyboardDidShow(notification: NSNotification) {
@@ -145,7 +171,18 @@ final class NewCommentViewController: UIViewController, UITextViewDelegate {
     // MARK: UITextViewDelegate
 
     func textViewDidChange(_ textView: UITextView) {
-        updateState()
+        updateTextState()
+    }
+
+    // MARK: AddCommentListener
+
+    func didSendComment(client: AddCommentClient, id: String, commentFields: CommentFields, reactionFields: ReactionFields) {
+        update(sending: false)
+        dismiss(animated: true)
+    }
+
+    func didFailSendingComment(client: AddCommentClient) {
+        update(sending: false)
     }
 
 }
