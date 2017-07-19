d43383d25f9141fce6a68854f3b6d32f87059e5f
Ryan Nystrom <rnystrom@instagram.com>
2017-07-19 18:11:57 -0400

    icon in placeholder VC, change color to white so its visible, fixes #154

--- Classes/View Controllers/SplitPlaceholderViewController.swift
+++ Classes/View Controllers/SplitPlaceholderViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import SnapKit
 
 final class SplitPlaceholderViewController: UIViewController {
+
+    private let imageView = UIImageView(image: UIImage(named: "splash-light"))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Styles.Colors.background
+
+        view.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
     }
 
 }
