2e5ff201eb53d32a7110148fe1351a16795b0076
Shaps Benkau <shapsuk@me.com>
2017-07-28 17:23:52 +0100

    Migrated GitHubAPI to be an Objective-C class to allow access to pre-processors. Removed secret.swift since its no longer required. Added GitHubAPI to bridging header.

---
+++ GithubAPI.h
@@ -0,0 +1,16 @@
+//
+//  GitHub.h
+//  Freetime
+//
+//  Created by Shahpour Benkau on 28/07/2017.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GithubAPI: NSObject
+    
++ (NSString * __nonnull)clientID;
++ (NSString * __nonnull)clientSecret;
+    
+@end
--- Resources/Freetime-Bridging-Header.h
+++ Resources/Freetime-Bridging-Header.h
@@ -3,3 +3,4 @@
 //
 
 #import "FlexController.h"
+#import "GithubAPI.h"
