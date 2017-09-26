5bcfb1962c5a893f0d75edc5d7ef88b85477fa5b
Ivan Magda <imagda15@gmail.com>
2017-09-27 01:36:07 +0300

    Create UIAlertController+Action #359
    UIAlertController+Action extension provide a convinient way for adding actions into the UIAlertController.

---
+++ Classes/Utility/UIAlertController+Action.swift
@@ -0,0 +1,25 @@
+//
+//  UIAlertController+AddAction.swift
+//  Freetime
+//
+//  Created by Ivan Magda on 26/09/2017.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+extension UIAlertController {
+    
+    func addActions(_ actions: [UIAlertAction?]) {
+        for anAction in actions {
+            self.add(action: anAction)
+        }
+    }
+    
+    func add(action: UIAlertAction?) {
+        if let action = action {
+            self.addAction(action)
+        }
+    }
+    
+}
