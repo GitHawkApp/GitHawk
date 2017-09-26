c001136f1b88308f6c28010be961749bbdc4741c
Ivan Magda <imagda15@gmail.com>
2017-09-27 01:33:24 +0300

    Create AlertActionBuilder #359
    The AlertActionBuilder is used to create custom UIAlertAction's with constituent parts.

---
+++ Classes/Utility/AlertActionBuilder.swift
@@ -0,0 +1,29 @@
+//
+//  AlertActionBuilder.swift
+//  Freetime
+//
+//  Created by Ivan Magda on 27/09/2017.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+// MARK: AlertActionBuilder -
+
+class AlertActionBuilder {
+    
+    typealias BuilderClosure = (AlertActionBuilder) -> ()
+    
+    // MARK: Properties
+    
+    var rootViewController: UIViewController?
+    var title: String?
+    var style: UIAlertActionStyle?
+    
+    // MARK: - Init
+    
+    init(_ buildClosure: BuilderClosure) {
+        buildClosure(self)
+    }
+    
+}
