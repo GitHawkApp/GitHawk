350677f16605f936b5121e1405f7a243be08d713
Ryan Nystrom <rnystrom@instagram.com>
2017-09-27 21:10:38 -0400

    add gql id decoder from #393

---
+++ Classes/Systems/GraphQLIDDecode.swift
@@ -0,0 +1,16 @@
+//
+//  GraphQLIDDecode.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 9/27/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import Foundation
+
+func GraphQLIDDecode(id: String, separator: String) -> Int? {
+    guard let data = Data(base64Encoded: id, options: [.ignoreUnknownCharacters]),
+    let text = String(data: data, encoding: .utf8)
+        else { return nil }
+    return (text.components(separatedBy: separator).last as NSString?)?.integerValue
+}
---
+++ FreetimeTests/GraphQLIDDecodeTests.swift
@@ -0,0 +1,18 @@
+//
+//  GraphQLIDDecodeTests.swift
+//  FreetimeTests
+//
+//  Created by Ryan Nystrom on 9/27/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import XCTest
+
+class GraphQLIDDecodeTests: XCTestCase {
+    
+    func test_whenDecodingIssueComment_thatNumberReturned() {
+        let result = GraphQLIDDecode(id: "MDEyOklzc3VlQ29tbWVudDMzMTY2OTI5OA==", separator: "IssueComment")
+        XCTAssertEqual(result, 331669298)
+    }
+    
+}
