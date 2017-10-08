95f05b61918e86a3983a4b3eff51cc123e070ad3
Weyert de Boer <weyert.deboer@keytree.co.uk>
2017-10-08 20:13:37 +0200

    Fixed a bunch of compiler warnings

--- Classes/Views/UIColor+Hex.swift
+++ Classes/Views/UIColor+Hex.swift
@@ -18,7 +18,7 @@ extension UIColor {
             cString.remove(at: cString.startIndex)
         }
 
-        if ((cString.characters.count) != 6) {
+        if ((cString.count) != 6) {
             return UIColor.gray
         }
 
