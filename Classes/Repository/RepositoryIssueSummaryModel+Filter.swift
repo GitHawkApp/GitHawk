e84321f28f894b483d24b8fde18b93dd0743b24b
Weyert de Boer <weyert.deboer@keytree.co.uk>
2017-10-11 22:37:23 +0200

    Improve the filtering of issues by changing the order

--- Classes/Repository/RepositoryIssueSummaryModel+Filter.swift
+++ Classes/Repository/RepositoryIssueSummaryModel+Filter.swift
@@ -15,27 +15,7 @@ func filterIssues(_ items: [ListDiffable], _ query: String) -> [ListDiffable] {
     let searchText = query.lowercased()
     let queryResults: SearchQueryParseResults = tokenizeSearchQuery(searchText)
 
-    let relevantItems = items.filter { item -> Bool in
-        // If every non RepositoryIssueSummaryModel type will never be filtered as they can be the head models
-        guard let summaryModel = item as? RepositoryIssueSummaryModel else { return true }
-
-        // If the search query is numeric then we shouldn't bother checking anything else
-        if searchText.rangeOfCharacter(from: CharacterSet.decimalDigits.inverted) == nil {
-            let ticketNumber = String(summaryModel.number)
-            return ticketNumber.contains(searchText)
-        }
-
-        let title = summaryModel.title.attributedText.string.lowercased()
-        if title.contains(queryResults.leftover) { return true }
-
-        let author = summaryModel.author.lowercased()
-        if author.contains(queryResults.leftover) { return true }
-
-        return false
-    }
-
-    // After we have determined the relevant items, we going to apply the token-based search
-    let filteredItems = relevantItems.filter { item -> Bool in
+    let filteredItems = items.filter { item -> Bool in
         // If every non RepositoryIssueSummaryModel type will never be filtered as they can be the head models
         guard let summaryModel = item as? RepositoryIssueSummaryModel else { return true }
         let filterResults = queryResults.tokens.flatMap { action, value -> Bool in
@@ -50,5 +30,28 @@ func filterIssues(_ items: [ListDiffable], _ query: String) -> [ListDiffable] {
         }
     }
 
-    return filteredItems
+    if queryResults.leftover.isEmpty {
+        return filteredItems
+    }
+
+    let relevantItems = filteredItems.filter { item -> Bool in
+        // If every non RepositoryIssueSummaryModel type will never be filtered as they can be the head models
+        guard let summaryModel = item as? RepositoryIssueSummaryModel else { return true }
+
+        // If the search query is numeric then we shouldn't bother checking anything else
+        if searchText.rangeOfCharacter(from: CharacterSet.decimalDigits.inverted) == nil {
+            let ticketNumber = String(summaryModel.number)
+            return ticketNumber.contains(queryResults.leftover)
+        }
+
+        let title = summaryModel.title.attributedText.string.lowercased()
+        if title.contains(queryResults.leftover) { return true }
+
+        let author = summaryModel.author.lowercased()
+        if author.contains(queryResults.leftover) { return true }
+
+        return false
+    }
+
+    return relevantItems
 }
