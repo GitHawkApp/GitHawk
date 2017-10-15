f38194401cec792e7d9bf30394ec8a94ead76e64
Ryan Nystrom <rnystrom@instagram.com>
2017-10-15 15:58:11 -0400

    fix build err

--- Classes/Systems/Feed.swift
+++ Classes/Systems/Feed.swift
@@ -80,7 +80,7 @@ final class Feed: NSObject, UIScrollViewDelegate {
     }
 
     func viewWillLayoutSubviews(view: UIView) {
-        let bounds = safeBounds
+        let bounds = view.safeBounds
 
         let changed = bounds != collectionView.frame
         if managesLayout && changed {
