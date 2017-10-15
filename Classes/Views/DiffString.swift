3e221d68d5eaa141b278d18f038b5fd8e6a611cc
Weyert de Boer <weyert.deboer@keytree.co.uk>
2017-10-16 01:56:22 +0200

    Added support for color coding code within language detection (handled by the Highlighr module)

--- Classes/Views/DiffString.swift
+++ Classes/Views/DiffString.swift
@@ -7,6 +7,49 @@
 //
 
 import UIKit
+import Highlightr
+
+func CreateColorCodedString(code: String, includeDiff: Bool=false, lineHandler: ((String, Int) -> LineChangeType)? = nil) -> NSAttributedString {
+    guard let highlightr = Highlightr() else { return NSAttributedString(string: code) }
+
+    highlightr.setTheme(to: "github")
+    let colorCodedString = highlightr.highlight(code, as: nil) ?? NSAttributedString(string: code)
+    let lines = code.components(separatedBy: CharacterSet.newlines)
+
+    if includeDiff {
+        let attributedText = NSMutableAttributedString(attributedString: colorCodedString)
+        attributedText.beginEditing()
+
+        // Forcefully update the font to our code font
+        var attributes = [
+            NSAttributedStringKey.font: Styles.Fonts.code,
+            NSAttributedStringKey.foregroundColor: Styles.Colors.Gray.dark.color
+        ]
+
+        // Ensure the all the text is by default our picked code font
+        attributedText.addAttributes(attributes, range: NSMakeRange(0, attributedText.string.count))
+
+        var lineIndex = 0
+        for line in lines {
+            let lineType = lineHandler?(line, lineIndex) ?? .unknown
+            if lineType == .addedLine {
+                attributes[NSAttributedStringKey.backgroundColor] = Styles.Colors.Green.light.color
+                attributedText.addAttributes(for: line, attributes: attributes, mode: .all)
+            } else if lineType == .deletedLine {
+                attributes[NSAttributedStringKey.backgroundColor] = Styles.Colors.Red.light.color
+                attributedText.addAttributes(for: line, attributes: attributes, mode: .all)
+            }
+
+            lineIndex += 1
+        }
+
+        attributedText.endEditing()
+
+        return attributedText
+    }
+
+    return colorCodedString
+}
 
 func CreateDiffString(code: String, limit: Bool = false) -> NSAttributedString {
     let split = code.components(separatedBy: CharacterSet.newlines)
