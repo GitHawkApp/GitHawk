03f6c0a3505225da15f529cd18607628d8aff880
Ryan Nystrom <rnystrom@instagram.com>
2017-10-22 00:07:20 -0400

    Disable git hash from build script

--- tools/bump_build_number.sh
+++ tools/bump_build_number.sh
@@ -14,6 +14,6 @@ dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
     /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $number_of_commits" "$plist"
-    /usr/libexec/PlistBuddy -c "Set :GitHawkHeadCommitHash ${git_release_version#*v}" "$plist"
+    # /usr/libexec/PlistBuddy -c "Set :GitHawkHeadCommitHash ${git_release_version#*v}" "$plist"
   fi
 done
