8ab933b39de69e93599074cc1249ed0bfb02e7bf
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2017-10-23 10:49:32 +0530

    [FIX] scroll to top for bookmarks screen

--- Classes/Bookmark/BookmarksViewController.swift
+++ Classes/Bookmark/BookmarksViewController.swift
@@ -106,7 +106,7 @@ TabNavRootViewControllerType {
     // MARK: TabNavRootViewControllerType
     
     func didSingleTapTab() {
-        tableView.setContentOffset(CGPoint.zero, animated: true)
+        tableView.scrollToTop(animated: true)
     }
     
     func didDoubleTapTab() {
