50b811667dbdcf2647788986ed496b3bb6e5e9bd
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2017-10-25 07:17:02 +0530

    [ADD] Focus title field when creating a new issue (#719)

--- Classes/New Issue/NewIssueTableViewController.swift
+++ Classes/New Issue/NewIssueTableViewController.swift
@@ -107,6 +107,12 @@ final class NewIssueTableViewController: UITableViewController, UITextFieldDeleg
         // Update title to use localization
         title = Constants.Strings.newIssue
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        titleField.becomeFirstResponder()
+    }
 
     // MARK: Private API
 
