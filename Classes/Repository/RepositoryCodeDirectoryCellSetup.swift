e43908c31e5e5a98595196e07ad362d539f21ff1
Nixsm <nichsmm@gmail.com>
2017-10-29 21:46:54 -0200

    Add extension file

---
+++ Classes/Repository/RepositoryCodeDirectoryCellSetup.swift
@@ -0,0 +1,9 @@
+//
+//  RepositoryCodeDirectoryCellSetup.swift
+//  Freetime
+//
+//  Created by Nicholas Meschke on 10/29/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import Foundation
