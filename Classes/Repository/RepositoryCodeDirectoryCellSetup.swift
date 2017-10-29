f1d2c336f2921393ef3c6c0a44367fbefec38f09
Nixsm <nichsmm@gmail.com>
2017-10-29 21:47:19 -0200

    Move code from StyledCell to an Extension file

--- Classes/Repository/RepositoryCodeDirectoryCellSetup.swift
+++ Classes/Repository/RepositoryCodeDirectoryCellSetup.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2017 Ryan Nystrom. All rights reserved.
 //
 
-import Foundation
+extension StyledTableCell {
+    
+    func setup(with file: RepositoryFile) {
+        textLabel?.text = file.name
+        
+        let imageName = file.isDirectory ? "file-directory" : "file"
+        imageView?.image = UIImage(named: imageName)?.withRenderingMode(.alwaysTemplate)
+        imageView?.tintColor = Styles.Colors.blueGray.color
+        accessoryType = file.isDirectory ? .disclosureIndicator : .none
+    }
+    
+}
