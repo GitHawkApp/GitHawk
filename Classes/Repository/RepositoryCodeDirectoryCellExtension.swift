fb41595aec7597996f5fe0f91bb0ce58db4567b4
Nixsm <nichsmm@gmail.com>
2017-10-29 22:00:58 -0200

    Add Code Repository Cell extension file

---
+++ Classes/Repository/RepositoryCodeDirectoryCellExtension.swift
@@ -0,0 +1,9 @@
+//
+//  RepositoryCodeDirectoryCellExtension.swift
+//  Freetime
+//
+//  Created by Nicholas Meschke on 10/29/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import Foundation
