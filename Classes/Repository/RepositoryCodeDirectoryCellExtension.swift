0a66ad3cb5e121878a0522e0639df124b5217914
Nixsm <nichsmm@gmail.com>
2017-10-29 22:01:17 -0200

    Move cell setup code to an extension of a StyledTableViewCell

--- Classes/Repository/RepositoryCodeDirectoryCellExtension.swift
+++ Classes/Repository/RepositoryCodeDirectoryCellExtension.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2017 Ryan Nystrom. All rights reserved.
 //
 
-import Foundation
+extension StyledTableCell {
+    
+    func setup(with file: RepositoryFile) {
+        textLabel?.text = file.name
+        
+        isAccessibilityElement = true
+        accessibilityTraits |= UIAccessibilityTraitButton
+        let fileType = file.isDirectory
+            ? NSLocalizedString("Directory", comment: "Used to specify the code cell is a directory.")
+            : NSLocalizedString("File", comment: "Used to specify the code cell is a file.")
+        accessibilityLabel = contentView.subviews
+            .flatMap { $0.accessibilityLabel }
+            .reduce("") { "\($0).\n\($1)" }
+            .appending(".\n\(fileType)")
+        accessibilityHint = file.isDirectory
+            ? NSLocalizedString("Shows the contents of the directory", comment: "")
+            : NSLocalizedString("Shows the contents of the file", comment: "")
+        
+        let imageName = file.isDirectory ? "file-directory" : "file"
+        imageView?.image = UIImage(named: imageName)?.withRenderingMode(.alwaysTemplate)
+        imageView?.tintColor = Styles.Colors.blueGray.color
+        accessoryType = file.isDirectory ? .disclosureIndicator : .none
+    }
+    
+}
--- Classes/Repository/RepositoryCodeDirectoryViewController.swift
+++ Classes/Repository/RepositoryCodeDirectoryViewController.swift
@@ -99,28 +99,10 @@ final class RepositoryCodeDirectoryViewController: UIViewController, UITableView
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! StyledTableCell
 
         let file = files[indexPath.row]
-        cell.textLabel?.text = file.name
-        
-        cell.isAccessibilityElement = true
-        cell.accessibilityTraits |= UIAccessibilityTraitButton
-        let fileType = file.isDirectory
-            ? NSLocalizedString("Directory", comment: "Used to specify the code cell is a directory.")
-            : NSLocalizedString("File", comment: "Used to specify the code cell is a file.")
-        cell.accessibilityLabel = cell.contentView.subviews
-            .flatMap { $0.accessibilityLabel }
-            .reduce("") { "\($0).\n\($1)" }
-            .appending(".\n\(fileType)")
-        cell.accessibilityHint = file.isDirectory
-            ? NSLocalizedString("Shows the contents of the directory", comment: "")
-            : NSLocalizedString("Shows the contents of the file", comment: "")
-
-        let imageName = file.isDirectory ? "file-directory" : "file"
-        cell.imageView?.image = UIImage(named: imageName)?.withRenderingMode(.alwaysTemplate)
-        cell.imageView?.tintColor = Styles.Colors.blueGray.color
-        cell.accessoryType = file.isDirectory ? .disclosureIndicator : .none
+        cell.setup(with: file)
 
         return cell
     }
