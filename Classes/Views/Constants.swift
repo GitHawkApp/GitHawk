cf2653c8847998dec7e3f2118ec37f0ce048c486
Bas Broek <bas@basbroek.nl>
2017-11-18 16:54:52 +0100

    PR review comments

--- Classes/Views/Constants.swift
+++ Classes/Views/Constants.swift
@@ -37,8 +37,8 @@ enum Constants {
         static let bullet = "\u{2022}"
         static let bulletHollow = "\u{25E6}"
         static let search = NSLocalizedString("Search", comment: "")
-        static let searchGitHub = "\(Constants.Strings.search) GitHub"
-        static let searchBookmarks = "\(Constants.Strings.search) \(Constants.Strings.bookmarks)"
+        static let searchGitHub = NSLocalizedString("Search GitHub", comment: "Used as a placeholder for the searchbar when searching for repositories.")
+        static let searchBookmarks = NSLocalizedString("Search Bookmarks", comment: "Used as a placeholder for the searchbar when searching within bookmarks.")
         static let clearAll = NSLocalizedString("Clear All", comment: "")
         static let delete = NSLocalizedString("Delete", comment: "")
         static let inbox = NSLocalizedString("Inbox", comment: "")
