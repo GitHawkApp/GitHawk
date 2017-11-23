788da386da87041acb5c920d66fe609359f820df
James Sherlock <James.Sherlock@sky.uk>
2017-11-23 20:40:12 +0000

    If at first you fail, wait 20 minutes for a build and then try again

--- Classes/Systems/Secrets.swift
+++ Classes/Systems/Secrets.swift
@@ -10,23 +10,28 @@ import Foundation
 
 enum Secrets {
     
+    enum CI {
+        static let githubId = "{GITHUBID}"
+        static let githubSecret = "{GITHUBSECRET}"
+        static let imgurId = "{IMGURID}"
+    }
+    
     enum GitHub {
-        static let clientId = Secrets.environmentVariable(named: "GITHUB_CLIENT_ID")
-        static let clientSecret = Secrets.environmentVariable(named: "GITHUB_CLIENT_SECRET")
+        static let clientId = Secrets.environmentVariable(named: "GITHUB_CLIENT_ID") ?? CI.githubId
+        static let clientSecret = Secrets.environmentVariable(named: "GITHUB_CLIENT_SECRET") ?? CI.githubSecret
     }
     
     enum Imgur {
-        static let clientId = Secrets.environmentVariable(named: "IMGUR_CLIENT_ID")
+        static let clientId = Secrets.environmentVariable(named: "IMGUR_CLIENT_ID") ?? CI.imgurId
     }
     
-    fileprivate static func environmentVariable(named: String) -> String {
+    fileprivate static func environmentVariable(named: String) -> String? {
         
         let processInfo = ProcessInfo.processInfo
         
         guard let value = processInfo.environment[named] else {
             print("‼️ Missing Environment Variable: '\(named)'")
-            print(UserDefaults.standard.value(forKey: named))
-            return ""
+            return nil
         }
         
         return value
