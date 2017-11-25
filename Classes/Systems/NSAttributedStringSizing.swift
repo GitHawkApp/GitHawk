4128f950a821f145890b49ed80fa69b40f48afc2
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2017-11-24 21:18:55 -0500

    Only keep one bitmap cache at a time (#1076)
    * only keep one bitmap cache at a time
    
    * add leftover changes

--- Classes/Systems/NSAttributedStringSizing.swift
+++ Classes/Systems/NSAttributedStringSizing.swift
@@ -130,9 +130,10 @@ final class NSAttributedStringSizing: NSObject, ListDiffable {
         layoutManager.addTextContainer(textContainer)
     }
 
-    private var _contents = [CGFloat: CGImage]()
+    private var _contents = [String: CGImage]()
     func contents(_ width: CGFloat) -> CGImage? {
-        if let contents = _contents[width] {
+        let key = "\(width)"
+        if let contents = _contents[key] {
             return contents
         }
 
@@ -148,7 +149,9 @@ final class NSAttributedStringSizing: NSObject, ListDiffable {
         let contents = UIGraphicsGetImageFromCurrentImageContext()?.cgImage
         UIGraphicsEndImageContext()
 
-        _contents[width] = contents
+        // keep only one bitmap at a time
+        _contents.removeAll()
+        _contents[key] = contents
 
         return contents
     }
