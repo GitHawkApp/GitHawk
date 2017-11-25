90cd6f478adc9fe92e5bb9046c789a4b378a82cb
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2017-11-24 20:52:35 -0500

    Milestone event cells use attributed text system (#1073)
    * move milestone attr string to model
    
    * address api changes for milestone model

--- Classes/Issues/MilestoneEvent/IssueMilestoneEventModel.swift
+++ Classes/Issues/MilestoneEvent/IssueMilestoneEventModel.swift
@@ -21,13 +21,61 @@ final class IssueMilestoneEventModel: ListDiffable {
     let milestone: String
     let date: Date
     let type: MilestoneType
+    let attributedText: NSAttributedStringSizing
 
-    init(id: String, actor: String, milestone: String, date: Date, type: MilestoneType) {
+    init(
+        id: String,
+        actor: String,
+        milestone: String,
+        date: Date,
+        type: MilestoneType,
+        width: CGFloat
+        ) {
         self.id = id
         self.actor = actor
         self.milestone = milestone
         self.date = date
         self.type = type
+
+        let attributedText = NSMutableAttributedString()
+        attributedText.append(NSAttributedString(
+            string: actor,
+            attributes: [
+                .font: Styles.Fonts.secondaryBold,
+                .foregroundColor: Styles.Colors.Gray.dark.color,
+                MarkdownAttribute.username: actor
+            ]
+        ))
+        let action: String
+        switch type {
+        case .milestoned: action = NSLocalizedString(" added to milestone ", comment: "")
+        case .demilestoned: action = NSLocalizedString(" removed from milestone ", comment: "")
+        }
+        attributedText.append(NSAttributedString(
+            string: action,
+            attributes: [
+                .foregroundColor: Styles.Colors.Gray.medium.color,
+                .font: Styles.Fonts.secondary
+            ]
+        ))
+        attributedText.append(NSAttributedString(
+            string: milestone,
+            attributes: [
+                .foregroundColor: Styles.Colors.Gray.dark.color,
+                .font: Styles.Fonts.secondaryBold
+            ]
+        ))
+        self.attributedText = NSAttributedStringSizing(
+            containerWidth: width,
+            attributedText: attributedText,
+            inset: UIEdgeInsets(
+                top: Styles.Sizes.inlineSpacing,
+                left: Styles.Sizes.eventGutter,
+                bottom: Styles.Sizes.inlineSpacing,
+                right: Styles.Sizes.eventGutter
+            ),
+            backgroundColor: Styles.Colors.background
+        )
     }
 
     // MARK: ListDiffable
