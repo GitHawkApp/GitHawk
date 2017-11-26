f08ee831b119b756daac16abc538a253daf6b3b3
Ryan Nystrom <rnystrom@instagram.com>
2017-11-26 12:23:22 -0500

    share code text view with repo and issue

--- Classes/Issues/Files/IssuePatchContentViewController.swift
+++ Classes/Issues/Files/IssuePatchContentViewController.swift
@@ -12,24 +12,13 @@ final class IssuePatchContentViewController: UIViewController {
 
     private let file: File
     private let client: GithubClient
-    private let scrollView = UIScrollView()
-    private let attributeView = AttributedStringView()
+    private let codeView = CodeView()
 
     init(file: File, client: GithubClient) {
         self.file = file
         self.client = client
         super.init(nibName: nil, bundle: nil)
-
         title = file.actualFileName
-
-        let width: CGFloat = 0
-        let text = NSAttributedStringSizing(
-            containerWidth: width,
-            attributedText: CreateDiffString(code: file.patch),
-            inset: Styles.Sizes.textViewInset
-        )
-        scrollView.contentSize = text.textViewSize(width)
-        attributeView.configureAndSizeToFit(text: text, width: width)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -38,18 +27,14 @@ final class IssuePatchContentViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .white
-
-        scrollView.isDirectionalLockEnabled = true
-        view.addSubview(scrollView)
-
-        scrollView.addSubview(attributeView)
+        view.addSubview(codeView)
+        codeView.set(attributedCode: CreateDiffString(code: file.patch))
     }
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        scrollView.frame = view.bounds
+        codeView.frame = view.bounds
     }
 
 }
---
+++ Classes/Views/CodeView.swift
@@ -0,0 +1,58 @@
+//
+//  CodeView.swift
+//  Freetime
+//
+//  Created by Ryan Nystrom on 11/26/17.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+final class CodeView: UIScrollView {
+
+    private lazy var textView: UITextView = {
+        let view = UITextView()
+        view.font = Styles.Fonts.code
+        view.isScrollEnabled = false
+        view.isEditable = false
+        view.contentInset = .zero
+        view.textContainerInset = UIEdgeInsets(
+            top: Styles.Sizes.rowSpacing,
+            left: Styles.Sizes.columnSpacing,
+            bottom: Styles.Sizes.rowSpacing,
+            right: Styles.Sizes.columnSpacing
+        )
+        self.addSubview(view)
+        return view
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+        isDirectionalLockEnabled = true
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Public API
+
+    func set(code: String) {
+        set(attributedCode: NSAttributedString(
+            string: code,
+            attributes: [
+                .font: Styles.Fonts.code,
+                .foregroundColor: Styles.Colors.Gray.dark.color,
+            ]))
+    }
+
+    func set(attributedCode: NSAttributedString) {
+        textView.attributedText = attributedCode
+        let max = CGFloat.greatestFiniteMagnitude
+        let size = textVi ew.sizeThatFits(CGSize(width: max, height: max))
+        textView.frame = CGRect(origin: .zero, size: size)
+        contentSize = size
+    }
+
+}
