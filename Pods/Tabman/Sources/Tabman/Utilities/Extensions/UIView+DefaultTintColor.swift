b3c0478a8308c835633aaa97d35f035e0e70fb60
Merrick Sapsford <merrick@sapsford.tech>
2017-12-05 16:09:23 +0100

    Update Tabman
    1.1.2

--- Pods/Tabman/Sources/Tabman/Utilities/Extensions/UIView+DefaultTintColor.swift
+++ Pods/Tabman/Sources/Tabman/Utilities/Extensions/UIView+DefaultTintColor.swift
@@ -1,9 +1,9 @@
 //
-//  ViewUtils.swift
+//  UIView+DefaultTintColor.swift
 //  Tabman
 //
-//  Created by Merrick Sapsford on 16/03/2017.
-//  Copyright © 2017 Merrick Sapsford. All rights reserved.
+//  Created by Merrick Sapsford on 05/12/2017.
+//  Copyright © 2017 UI At Six. All rights reserved.
 //
 
 import UIKit
