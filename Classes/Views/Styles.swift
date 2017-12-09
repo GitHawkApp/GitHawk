dc5becb24fd690bcb240a34ad26726a1167b7f39
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2017-12-09 12:31:23 -0500

    Dynamic content size support (#1206)
    * add multiplier based on device text size
    
    * adjust settings labels
    
    * syntax highlighting supported

--- Classes/Views/Styles.swift
+++ Classes/Views/Styles.swift
@@ -48,18 +48,47 @@ enum Styles {
         static let labelTextPadding: CGFloat = 4
         
         enum Text {
-            static let body: CGFloat = 16
-            static let secondary: CGFloat = 13
-            static let title: CGFloat = 14
-            static let button: CGFloat = 16
-            static let headline: CGFloat = 18
-            static let smallTitle: CGFloat = 12
-            static let h1: CGFloat = 24
-            static let h2: CGFloat = 22
-            static let h3: CGFloat = 20
-            static let h4: CGFloat = 18
-            static let h5: CGFloat = 16
-            static let h6: CGFloat = 16
+
+            private static func size(
+                _ base: CGFloat,
+                minSize: CGFloat = 0,
+                maxSize: CGFloat = CGFloat.greatestFiniteMagnitude
+                ) -> CGFloat {
+                let multiplier: CGFloat
+                // https://github.com/mapbox/mapbox-navigation-ios/blob/master/MapboxNavigation/UIFont.swift#L8-L19
+                switch UIApplication.shared.preferredContentSizeCategory {
+                case .accessibilityExtraExtraExtraLarge: multiplier = 23 / 16
+                case .accessibilityExtraExtraLarge: multiplier = 22 / 16
+                case .accessibilityExtraLarge: multiplier = 21 / 16
+                case .accessibilityLarge: multiplier = 20 / 16
+                case .accessibilityMedium: multiplier = 19 / 16
+                case .extraExtraExtraLarge: multiplier = 19 / 16
+                case .extraExtraLarge: multiplier = 18 / 16
+                case .extraLarge: multiplier = 17 / 16
+                case .large: multiplier = 1
+                case .medium: multiplier = 15 / 16
+                case .small: multiplier = 14 / 16
+                case .extraSmall: multiplier = 13 / 16
+                default: multiplier = 1
+                }
+                let result = base * multiplier
+                return min(max(result, minSize), maxSize)
+            }
+
+            static var body: CGFloat { return size(16) }
+            static var secondary: CGFloat { return size(13) }
+            static var title: CGFloat { return size(14) }
+            static var button: CGFloat { return size(16) }
+            static var headline: CGFloat { return size(18) }
+            static var smallTitle: CGFloat { return size(12) }
+            static var h1: CGFloat { return size(24) }
+            static var h2: CGFloat { return size(22) }
+            static var h3: CGFloat { return size(20) }
+            static var h4: CGFloat { return size(18) }
+            static var h5: CGFloat { return size(16) }
+            static var h6: CGFloat { return size(16) }
+            static var code: CGFloat { return size(14) }
+
         }
 
         enum HTML {
