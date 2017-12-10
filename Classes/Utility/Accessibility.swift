e26bdbee732b96029c93730f7dd22a1459fcb292
Bas Broek <bas@basbroek.nl>
2017-12-10 20:24:36 +0100

    Don't reduce if only one label

--- Classes/Utility/Accessibility.swift
+++ Classes/Utility/Accessibility.swift
@@ -28,8 +28,11 @@ enum AccessibilityHelper {
 
 private extension AccessibilityHelper {
     static func generatedLabel(forContentViewContainer cell: ContentViewContaining) -> String {
-        return cell.contentView.subviews
-            .flatMap { $0.accessibilityLabel }
-            .reduce("") { "\($0).\n\($1)" }
+        let labels = cell.contentView.subviews.flatMap { $0.accessibilityLabel }
+        if labels.count == 1, let label = labels.first {
+            return label
+        } else {
+            return labels.reduce("") { "\($0).\n\($1)" }
+        }
     }
 }
