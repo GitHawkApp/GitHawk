5ce70249baac74df7d8b74af2b4a90e7b486e76b
Ryan Nystrom <rnystrom@instagram.com>
2017-12-31 20:53:11 -0500

    move autocomplete stuff to its own controller + API

---
+++ Local Pods/MessageViewController/MessageAutocompleteController.swift
@@ -0,0 +1,153 @@
+//
+//  MessageAutocompleteController.swift
+//  MessageViewController
+//
+//  Created by Ryan Nystrom on 12/31/17.
+//
+
+import UIKit
+
+public protocol MessageAutocompleteControllerDelegate: class {
+    func didFind(controller: MessageAutocompleteController, prefix: String, word: String)
+}
+
+public protocol MessageAutocompleteControllerLayoutDelegate: class {
+    func needsLayout(controller: MessageAutocompleteController)
+}
+
+public final class MessageAutocompleteController: MessageTextViewListener {
+
+    public let textView: MessageTextView
+    public let autocompleteTableView = UITableView()
+
+    public weak var delegate: MessageAutocompleteControllerDelegate?
+    public weak var layoutDelegate: MessageAutocompleteControllerLayoutDelegate?
+
+    public struct CurrentAutocomplete {
+        public let prefix: String
+        public let word: String
+        public let range: NSRange
+    }
+    public private(set) var currentAutocomplete: CurrentAutocomplete?
+
+    internal var registeredPrefixes = Set<String>()
+    internal let autocompleteBorder = CALayer()
+    internal var keyboardHeight: CGFloat = 0
+
+    init(textView: MessageTextView, layoutDelegate: MessageAutocompleteControllerLayoutDelegate) {
+        self.textView = textView
+        textView.add(listener: self)
+
+        self.layoutDelegate = layoutDelegate
+
+        autocompleteTableView.isHidden = true
+
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(keyboardWillChangeFrame(notification:)),
+            name: NSNotification.Name.UIKeyboardWillChangeFrame,
+            object: nil
+        )
+    }
+
+    // MARK: Public API
+
+    public final func register(prefix: String) {
+        registeredPrefixes.insert(prefix)
+    }
+
+    public final func showAutocomplete(_ doShow: Bool) {
+        if doShow {
+            autocompleteTableView.reloadData()
+            autocompleteTableView.layoutIfNeeded()
+        }
+        autocompleteTableView.isHidden = !doShow
+        autocompleteBorder.isHidden = !doShow
+    }
+
+    public final func accept(autocomplete: String, keepPrefix: Bool = true) {
+        defer { cancelAutocomplete() }
+
+        guard let current = currentAutocomplete,
+            let text = textView.text
+            else { return }
+
+        let prefixLength = current.prefix.utf16.count
+        let insertionRange = NSRange(
+            location: current.range.location + (keepPrefix ? prefixLength : 0),
+            length: current.word.utf16.count + (!keepPrefix ? prefixLength : 0)
+        )
+
+        guard let range = Range(insertionRange, in: text) else { return }
+
+        textView.text = text.replacingCharacters(in: range, with: autocomplete)
+        textView.selectedRange = NSRange(
+            location: insertionRange.location + autocomplete.utf16.count,
+            length: 0
+        )
+    }
+
+    internal func cancelAutocomplete() {
+        currentAutocomplete = nil
+        showAutocomplete(false)
+    }
+
+    public final var autocompleteMaxVisibleHeight: CGFloat = 200 {
+        didSet { layoutDelegate?.needsLayout(controller: self) }
+    }
+
+    public func layout(in view: UIView, bottomY: CGFloat? = nil) {
+        if autocompleteTableView.superview != view {
+            view.addSubview(autocompleteTableView)
+            view.layer.addSublayer(autocompleteBorder)
+        }
+
+        let bounds = view.bounds
+        let pinY = bottomY ?? (bounds.height - keyboardHeight)
+
+        let autocompleteHeight = min(autocompleteMaxVisibleHeight, autocompleteTableView.contentSize.height)
+        let autocompleteFrame = CGRect(
+            x: bounds.minX,
+            y: pinY - autocompleteHeight,
+            width: bounds.width,
+            height: autocompleteHeight
+        )
+        autocompleteTableView.frame = autocompleteFrame
+
+        let borderHeight = 1 / UIScreen.main.scale
+        UIView.performWithoutAnimation {
+            autocompleteBorder.frame = CGRect(
+                x: bounds.minX,
+                y: autocompleteFrame.minY - borderHeight,
+                width: bounds.width,
+                height: borderHeight
+            )
+        }
+    }
+
+    // MARK: Private API
+
+    internal func checkForAutocomplete() {
+        guard let result = textView.find(prefixes: registeredPrefixes) else {
+            cancelAutocomplete()
+            return
+        }
+        let wordWithoutPrefix = (result.word as NSString).substring(from: result.prefix.utf16.count)
+        currentAutocomplete = CurrentAutocomplete(prefix: result.prefix, word: wordWithoutPrefix, range: result.range)
+
+        delegate?.didFind(controller: self, prefix: result.prefix, word: wordWithoutPrefix)
+    }
+
+    @objc internal func keyboardWillChangeFrame(notification: Notification) {
+        guard let keyboardFrame = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? CGRect
+            else { return }
+        keyboardHeight = keyboardFrame.height
+    }
+
+    // MARK: MessageTextViewListener
+
+    public func didChange(textView: MessageTextView) {}
+    public func didChangeSelection(textView: MessageTextView) {}
+
+}
