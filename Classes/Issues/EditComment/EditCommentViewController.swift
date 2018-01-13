60bcee46580496b0b300b3af8f46517da5092204
Bas Broek <BasThomas@users.noreply.github.com>
2018-01-13 21:55:42 +0100

    Make edit comment keyboard use the Twitter style (#1398)

--- Classes/Issues/EditComment/EditCommentViewController.swift
+++ Classes/Issues/EditComment/EditCommentViewController.swift
@@ -68,6 +68,7 @@ MessageTextViewListener {
         )
 
         textView.githawkConfigure(inset: true)
+        textView.keyboardType = .twitter
         view.addSubview(textView)
         textView.snp.makeConstraints { make in
             make.edges.equalTo(view)
