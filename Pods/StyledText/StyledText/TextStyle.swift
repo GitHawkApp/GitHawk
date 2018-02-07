220a5a3373fba8a8a250e2a3df5a20373c7334dd
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2018-02-07 10:39:18 -0500

    update StyledText and fix semibold fonts (#1497)

--- Pods/StyledText/StyledText/TextStyle.swift
+++ Pods/StyledText/StyledText/TextStyle.swift
@@ -10,12 +10,31 @@ import UIKit
 
 public struct TextStyle: Hashable, Equatable {
 
-    public let name: String
+    public let fontDescriptor: UIFontDescriptor
     public let size: CGFloat
     public let attributes: [NSAttributedStringKey: Any]
-    public let traits: UIFontDescriptorSymbolicTraits
     public let minSize: CGFloat
     public let maxSize: CGFloat
+
+    public init(
+        fontDescriptor: UIFontDescriptor = UIFont.systemFont(ofSize: 1).fontDescriptor,
+        size: CGFloat = UIFont.systemFontSize,
+        attributes: [NSAttributedStringKey: Any] = [:],
+        minSize: CGFloat = 0,
+        maxSize: CGFloat = .greatestFiniteMagnitude
+        ) {
+        self.fontDescriptor = fontDescriptor
+        self.size = size
+        self.attributes = attributes
+        self.minSize = minSize
+        self.maxSize = maxSize
+
+        self._hashValue = fontDescriptor
+            .combineHash(with: size)
+            .combineHash(with: attributes.count)
+            .combineHash(with: minSize)
+            .combineHash(with: maxSize)
+    }
 
     public init(
         name: String = UIFont.systemFont(ofSize: 1).fontName,
@@ -25,25 +44,25 @@ public struct TextStyle: Hashable, Equatable {
         minSize: CGFloat = 0,
         maxSize: CGFloat = .greatestFiniteMagnitude
         ) {
-        self.name = name
-        self.size = size
-        self.attributes = attributes
-        self.traits = traits
-        self.minSize = minSize
-        self.maxSize = maxSize
 
-        self._hashValue = name
-            .combineHash(with: size)
-            .combineHash(with: attributes.count)
-            .combineHash(with: traits.rawValue)
-            .combineHash(with: minSize)
-            .combineHash(with: maxSize)
+        let baseFontDescriptor = UIFontDescriptor()
+            .addingAttributes([.name: name])
+        let traitFontDescriptor = baseFontDescriptor.withSymbolicTraits(traits)
+        if traitFontDescriptor == nil {
+            print("WARNING: No font found for name \"\(name)\" with traits \(traits)")
+        }
+        self.init(
+            fontDescriptor: traitFontDescriptor ?? UIFontDescriptor(name: name, size: size),
+            size: size,
+            attributes: attributes,
+            minSize: minSize,
+            maxSize: maxSize
+        )
     }
 
     public func font(contentSizeCategory: UIContentSizeCategory) -> UIFont {
         let preferredSize = contentSizeCategory.preferredContentSize(size)
-        let font = UIFont(name: name, size: preferredSize) ?? UIFont.systemFont(ofSize: preferredSize)
-        return font.addingTraits(traits: traits)
+        return UIFont(descriptor: fontDescriptor, size: preferredSize)
     }
 
     internal let _hashValue: Int
@@ -53,10 +72,9 @@ public struct TextStyle: Hashable, Equatable {
 
     public static func == (lhs: TextStyle, rhs: TextStyle) -> Bool {
         return lhs.size == rhs.size
-            && lhs.traits == rhs.traits
             && lhs.minSize == rhs.minSize
             && rhs.maxSize == rhs.maxSize
-            && lhs.name == rhs.name
+            && lhs.fontDescriptor == rhs.fontDescriptor
             && NSDictionary(dictionary: lhs.attributes).isEqual(to: rhs.attributes)
     }
 
