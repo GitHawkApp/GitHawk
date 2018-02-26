69ba45df6b0e3ebf30538de2b5a673d2518dbf60
Ryan Nystrom <rnystrom@instagram.com>
2018-02-25 22:29:33 -0500

    update notification cells when ago string changes

--- Classes/Notifications/NotificationViewModel.swift
+++ Classes/Notifications/NotificationViewModel.swift
@@ -31,6 +31,7 @@ final class NotificationViewModel: ListDiffable, Cachable {
     let title: NSAttributedStringSizing
     let type: NotificationType
     let date: Date
+    let agoString: String
     let read: Bool
     let owner: String
     let repo: String
@@ -60,6 +61,7 @@ final class NotificationViewModel: ListDiffable, Cachable {
         self.identifier = identifier
         self.state = state
         self.commentCount = commentCount
+        self.agoString = date.agoString
     }
 
     convenience init(
@@ -135,7 +137,7 @@ final class NotificationViewModel: ListDiffable, Cachable {
         guard let object = object as? NotificationViewModel else { return false }
         return read == object.read
             && type == object.type
-            && date == object.date
+            && agoString == object.agoString
             && repo == object.repo
             && owner == object.owner
             && state == object.state
