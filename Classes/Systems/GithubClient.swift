a5a2c48f68f8fdbbc86912b82227b3a0003854cc
Ryan Nystrom <rnystrom@instagram.com>
2018-03-04 01:05:17 -0500

    delete unused request from client

--- Classes/Systems/GithubClient.swift
+++ Classes/Systems/GithubClient.swift
@@ -15,55 +15,6 @@ import GitHubAPI
 
 struct GithubClient {
 
-    struct Page {
-        let next: Int
-        let last: Int
-    }
-
-    struct Request {
-
-        let url: String
-        let method: Alamofire.HTTPMethod
-        let parameters: Parameters?
-        let headers: HTTPHeaders?
-        let logoutOnAuthFailure: Bool
-        let completion: (DataResponse<Any>, Page?) -> Void
-
-        init(
-            path: String,
-            method: Alamofire.HTTPMethod = .get,
-            parameters: Parameters? = nil,
-            headers: HTTPHeaders? = nil,
-            logoutOnAuthFailure: Bool = true,
-            completion: @escaping (DataResponse<Any>, Page?) -> Void
-            ) {
-            self.init(
-                url: "https://api.github.com/" + path,
-                method: method,
-                parameters: parameters,
-                headers: headers,
-                logoutOnAuthFailure: logoutOnAuthFailure,
-                completion: completion
-            )
-        }
-
-        init(
-            url: String,
-            method: Alamofire.HTTPMethod = .get,
-            parameters: Parameters? = nil,
-            headers: HTTPHeaders? = nil,
-            logoutOnAuthFailure: Bool = true,
-            completion: @escaping (DataResponse<Any>, Page?) -> Void
-            ) {
-            self.url = url
-            self.method = method
-            self.parameters = parameters
-            self.headers = headers
-            self.logoutOnAuthFailure = logoutOnAuthFailure
-            self.completion = completion
-        }
-    }
-
     let sessionManager: GithubSessionManager
     let apollo: ApolloClient
     let networker: Alamofire.SessionManager
@@ -90,58 +41,6 @@ struct GithubClient {
         } else {
             self.bookmarksStore = nil
         }
-    }
-
-    @discardableResult
-    func request(
-        _ request: GithubClient.Request
-        ) -> DataRequest {
-        print("Requesting \(request.method.rawValue): \(request.url)")
-
-        let encoding: ParameterEncoding
-        switch request.method {
-        case .get: encoding = URLEncoding.queryString
-        default: encoding = JSONEncoding.default
-        }
-
-        var parameters = request.parameters ?? [:]
-        parameters["access_token"] = userSession?.token
-
-        return networker.request(request.url,
-                                 method: request.method,
-                                 parameters: parameters,
-                                 encoding: encoding,
-                                 headers: request.headers)
-            .responseJSON(completionHandler: { response in
-                // remove the github session if requesting with a session
-                if request.logoutOnAuthFailure,
-                    let statusCode = response.response?.statusCode,
-                    statusCode == 401 {
-                    ToastManager.showRevokeError()
-                    self.sessionManager.logout()
-                } else {
-                    let page = PagingData(link: response.response?.allHeaderFields["Link"] as? String)
-                    request.completion(response, page)
-                }
-            })
-    }
-
-    @discardableResult
-    func graphQL(
-        parameters: Parameters,
-        completion: @escaping (DataResponse<Any>) -> Void
-        ) -> DataRequest {
-        let encoding: ParameterEncoding = JSONEncoding.default
-        return networker.request(
-            "https://api.github.com/graphql",
-            method: .post,
-            parameters: parameters,
-            encoding: encoding,
-            headers: ["Authorization": "bearer " + (userSession?.token ?? "")]
-            )
-            .responseJSON(completionHandler: { response in
-                completion(response)
-            })
     }
 
     @discardableResult
