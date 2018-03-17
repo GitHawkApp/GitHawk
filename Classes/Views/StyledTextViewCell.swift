f64751cce81a974c433be533d43b0a41ba055250
Ryan Nystrom <rnystrom@instagram.com>
2018-03-17 19:49:08 -0400

    styled cell interaction working

--- Classes/Views/StyledTextViewCell.swift
+++ Classes/Views/StyledTextViewCell.swift
@@ -15,7 +15,7 @@ protocol StyledTextViewCellDelegate: class {
 
 class StyledTextViewCell: UICollectionViewCell, StyledTextViewDelegate {
 
-    
+    weak var delegate: StyledTextViewCellDelegate?
 
     private let textView = StyledTextView()
 
@@ -49,11 +49,26 @@ class StyledTextViewCell: UICollectionViewCell, StyledTextViewDelegate {
     // MARK: StyledTextViewDelegate
 
     func didTap(view: StyledTextView, attributes: [NSAttributedStringKey: Any], point: CGPoint) {
-
+        guard let detected = DetectMarkdownAttribute(attributes: attributes) else { return }
+        delegate?.didTap(cell: self, attribute: detected)
     }
 
     func didLongPress(view: StyledTextView, attributes: [NSAttributedStringKey: Any], point: CGPoint) {
-
+        if let details = attributes[MarkdownAttribute.details] as? String {
+            showDetailsInMenu(details: details, point: point)
+        }
     }
+
+    @objc func showDetailsInMenu(details: String, point: CGPoint) {
+        becomeFirstResponder()
+        let menu = UIMenuController.shared
+        menu.menuItems = [
+            UIMenuItem(title: details, action: #selector(StyledTextViewCell.empty))
+        ]
+        menu.setTargetRect(CGRect(origin: point, size: CGSize(width: 1, height: 1)), in: self)
+        menu.setMenuVisible(true, animated: trueUnlessReduceMotionEnabled)
+    }
+
+    @objc func empty() {}
 
 }
