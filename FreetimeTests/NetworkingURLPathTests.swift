a73b0f61df68f804e629beb9bdfda38f74f35c29
Brian Litwin <brian.n.litwin@gmail.com>
2018-05-29 13:26:34 -0400

    updated urlpath tests

--- FreetimeTests/NetworkingURLPathTests.swift
+++ FreetimeTests/NetworkingURLPathTests.swift
@@ -11,44 +11,20 @@ import XCTest
 @testable import Freetime
 
 class UrlPathComponents: XCTestCase {
+    //Issue #1829: add reviewers was receiving a 404 response
     
-    func DISABLED_test_addReviewers() {
-        
-        //test that add reviewers request gets a 401 unauthorized response instead of a 404
-        //Issue #1829: add reviewers was receiving a 404 response
+    func test_addReviewers() {
         let addReviewersRequest = V3AddPeopleRequest(owner: "GitHawkApp",
                                                      repo: "GitHawk",
                                                      number: 1549,
                                                      type: .reviewers,
                                                      add: true,
                                                      people: [])
-        
-        let githubclient = newGithubClient()
-        let promise = expectation(description: "completion handler invoked")
-        var requestResponse: GitHubAPI.Result<V3StatusCodeResponse<V3StatusCode200or201>>?
-        
-        githubclient.client.send(addReviewersRequest) { response in
-            requestResponse = response
-            promise.fulfill()
-        }
-        
-        waitForExpectations(timeout: 5, handler: nil)
-        
-        switch requestResponse! {
-        case .failure(let error):
-            let clientError = error as! GitHubAPI.ClientError
-            switch clientError {
-            case .unauthorized: break //test passed i.e. received a 401 response
-            default: XCTFail("Client Error hould be .unauthorized i.e. 401 response")
-            }
-        default:
-            XCTFail("Response should be ResponseType.failure(GitHubAPI.ClientError.unauthorized) i.e. 401 response")
-        }
+        XCTAssertEqual(addReviewersRequest.url,
+                       "https://api.github.com/repos/GitHawkApp/GitHawk/pulls/1549/requested_reviewers")
     }
     
-    func DISABLED_test_addAssignees() {
-        
-        //test that add reviewers request gets a 401 unauthorized response
+    func test_addAssignees() {
         let addAssigneesRequest = V3AddPeopleRequest(owner: "GitHawkApp",
                                                      repo: "GitHawk",
                                                      number: 1549,
@@ -56,27 +32,8 @@ class UrlPathComponents: XCTestCase {
                                                      add: true,
                                                      people: [])
         
-        let githubclient = newGithubClient()
-        let promise = expectation(description: "completion handler invoked")
-        var requestResponse: GitHubAPI.Result<V3StatusCodeResponse<V3StatusCode200or201>>?
-        
-        githubclient.client.send(addAssigneesRequest) { response in
-            requestResponse = response
-            promise.fulfill()
-        }
-        
-        waitForExpectations(timeout: 5, handler: nil)
-        
-        switch requestResponse! {
-        case .failure(let error):
-            let clientError = error as! GitHubAPI.ClientError
-            switch clientError {
-            case .unauthorized: break //test passed i.e. received a 401 response
-            default: XCTFail("Client Error should be .unauthorized i.e. 401 response")
-            }
-        default:
-            XCTFail("Response should be ResponseType.failure(GitHubAPI.ClientError.unauthorized) i.e. 401 response")
-        }
+        XCTAssertEqual(addAssigneesRequest.url,
+                       "https://api.github.com/repos/GitHawkApp/GitHawk/issues/1549/assignees")
     }
 }
 
