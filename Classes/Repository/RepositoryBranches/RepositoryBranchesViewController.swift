cfdd6d138867aa141e559559371fdee02ad42d94
Brian Litwin <brian.n.litwin@gmail.com>
2018-10-04 19:27:17 -0400

    Order repo branches after fetch (#2228)
    * Order repo branches after fetch
    - added func to order branches + unit tests
    - replaced 'var branch: String' w/ 'private(set) var selectedBranch: String'
    & added a defaultBranch variable
    - changed fetch(page:) to update(animated:) in didSelect(value:) - an oversight from original pr
    
    * AX animation

--- Classes/Repository/RepositoryBranches/RepositoryBranchesViewController.swift
+++ Classes/Repository/RepositoryBranches/RepositoryBranchesViewController.swift
@@ -19,15 +19,18 @@ RepositoryBranchSectionControllerDelegate
     private let repo: String
     private let client: GithubClient
     private var branches: [String] = []
-    public var branch: String
+    private let defaultBranch: String
+    public private(set) var selectedBranch: String
     
-    init(branch: String,
+    init(defaultBranch: String,
+         selectedBranch: String,
          owner: String,
          repo: String,
          client: GithubClient
         )
     {
-        self.branch = branch
+        self.defaultBranch = defaultBranch
+        self.selectedBranch = selectedBranch
         self.owner = owner
         self.repo = repo
         self.client = client
@@ -46,6 +49,23 @@ RepositoryBranchSectionControllerDelegate
         addMenuDoneButton()
     }
     
+    static func arrangeBranches(selectedBranch: String, defaultBranch: String, branches: [String]) -> [String] {
+        var branches = branches
+        
+        if let selectedBranchIndex = branches.index(of: selectedBranch) {
+            let elem = branches.remove(at: selectedBranchIndex)
+            branches.insert(elem, at: 0)
+        }
+        
+        if defaultBranch != selectedBranch,
+            let defaultBranchIndex = branches.index(of: defaultBranch) {
+            let elem = branches.remove(at: defaultBranchIndex)
+            branches.insert(elem, at: 1)
+        }
+        
+        return branches
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -55,9 +75,15 @@ RepositoryBranchSectionControllerDelegate
                                  repo: repo
             )
         {  [weak self] result in
+            guard let strongSelf = self else { return }
             switch result {
             case .success(let branches):
-                self?.branches = branches
+                self?.branches = RepositoryBranchesViewController.arrangeBranches(
+                    selectedBranch: strongSelf.selectedBranch,
+                    defaultBranch: strongSelf.defaultBranch,
+                    branches: branches
+                )
+                
             case .error:
                 Squawk.showError(message: "Couldn't fetch repository branches")
             }
@@ -69,7 +95,7 @@ RepositoryBranchSectionControllerDelegate
         guard feed.status == .idle else { return [] }
         return branches.map {
             let value = RepositoryBranchViewModel(branch: $0,
-                                                  selected: $0 == self.branch)
+                                                  selected: $0 == self.selectedBranch)
             
             return ListSwiftPair(value) { [weak self] in
                 let controller = RepositoryBranchSectionController()
@@ -80,7 +106,7 @@ RepositoryBranchSectionControllerDelegate
     }
     
     func didSelect(value: RepositoryBranchViewModel) {
-        self.branch = value.branch
-        fetch(page: nil)
+        self.selectedBranch = value.branch
+        update(animated: trueUnlessReduceMotionEnabled)
     }
 }
