76d4ec27a4e532c4f83d6684e85a2252b3d20822
Ehud Adler <easports96@gmail.com>
2018-11-05 16:56:23 -0500

    Implement review notes(2)

--- Classes/New Issue/NewIssueTableViewController.swift
+++ Classes/New Issue/NewIssueTableViewController.swift
@@ -51,7 +51,7 @@ final class NewIssueTableViewController: UITableViewController, UITextFieldDeleg
     @IBOutlet var titleField: UITextField!
     @IBOutlet var bodyField: UITextView!
 
-    private var template: String = ""
+    private var template = ""
     private var client: GithubClient!
     private var owner: String!
     private var repo: String!
