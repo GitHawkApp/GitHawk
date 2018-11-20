f2c65630167603666b5ac56cd512cd54b9b348f9
Ehud Adler <easports96@gmail.com>
2018-11-19 22:53:30 -0500

    Added a test

--- FreetimeTests/TemplateRegexTests.swift
+++ FreetimeTests/TemplateRegexTests.swift
@@ -69,6 +69,32 @@ class TemplateRegexTests: XCTestCase {
 
     }
 
+    func test_getNameAndDescriptionFromTemplateDetails_Test2() {
+        let validStr = """
+        -----
+        name: foo
+        about: bar
+        -----
+        name: baz
+        about: foobar
+        """
+
+        let names = validStr.matches(regex: String.getRegexForLine(after: "name"))
+        let name = names.first?.trimmingCharacters(in: .whitespaces)
+
+        let abouts = validStr.matches(regex: String.getRegexForLine(after: "about"))
+        let about = abouts.first?.trimmingCharacters(in: .whitespaces)
+
+        let nameAndDescription = IssueTemplateHelper.getNameAndDescription(fromTemplatefile: validStr)
+
+        XCTAssertEqual(name, "foo")
+        XCTAssertEqual(about, "bar")
+
+        XCTAssertEqual(nameAndDescription.name, "foo")
+        XCTAssertEqual(nameAndDescription.about, "bar")
+
+    }
+
     func test_cleaningUpDetailsFromTemplate() {
         let template = """
         -----
@@ -92,7 +118,6 @@ class TemplateRegexTests: XCTestCase {
         }
 
         XCTAssertEqual(cleanedFile, "Lorem ipsum dolor sit amet, mei no ignota commodo, dolorem luptatum in qui. Iusto regione inermis ea quo, te eos consul utroque scaevola. Ea per possim salutandi. At tota aperiam ius, facilisi recusabo accommodare vel cu. Cibo omnium utamur et pro, :apple: temporibus eu cum.\nCum paulo definitionem at, vel atqui tempor ad. Ex minimum percipit erroribus vis, qui erroribus persequeris te, et augue :chart_with_upwards_trend: mediocritatem eum. Et mei cibo dicta, vis reque :dancers: at, suas mutat corrumpit eos eu. Sint eruditi eos ei, ad pri esse omnes, dolorem blandit voluptatibus an ius.\nOption eligendi id eum, ne sea porro :small_red_triangle_down: assueverit, sed modo diam in. Id vim eripuit intellegebat, ad quo consul constituto, at vix natum interpretaris. Id his tritani vituperata, ea minim quidam aliquip vis, animal pertinacia ad sit. Mel impedit")
-
     }
 
 }
