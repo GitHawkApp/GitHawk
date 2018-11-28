207593afc7f2e989b9107d6ff807f9d137cb89fc
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2018-11-27 21:34:45 -0500

    fix tests

--- FreetimeTests/Search Tests/SearchRecentViewModelTests.swift
+++ FreetimeTests/Search Tests/SearchRecentViewModelTests.swift
@@ -21,7 +21,7 @@ class SearchRecentViewModelTests: XCTestCase {
     override func setUp() {
         super.setUp()
         searchQuery = SearchQuery.search("Pythonic")
-        let kickstarter = RepositoryDetails(owner: "Kickstarter", name: "ios-oss", defaultBranch: "master", hasIssuesEnabled: true)
+        let kickstarter = RepositoryDetails(owner: "Kickstarter", name: "ios-oss")
         recentlyViewedQuery = SearchQuery.recentlyViewed(kickstarter)
 
         searchViewModel = SearchRecentViewModel(query: searchQuery)
