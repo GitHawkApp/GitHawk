f23567ed39d10163e0a61f432ae5c492de041956
Richard Lu <luxh93@gmail.com>
2019-05-03 15:59:16 -0400

    update bookmarks to SwipeSelectableCell

--- Classes/Bookmark/BookmarkCell.swift
+++ Classes/Bookmark/BookmarkCell.swift
@@ -10,7 +10,7 @@ import UIKit
 import SnapKit
 import StyledTextKit
 
-final class BookmarkCell: SelectableCell {
+final class BookmarkCell: SwipeSelectableCell {
 
     static let titleInset = UIEdgeInsets(
         top: Styles.Sizes.rowSpacing,
